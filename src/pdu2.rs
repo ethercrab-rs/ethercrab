7eb7330a0a1a910c6b8b1667b23789d6b95d8fc6
James Waples <james@wapl.es>
2022-06-16 22:23:52 +0100

    Parse response PDU

--- src/pdu2.rs
+++ src/pdu2.rs
@@ -5,11 +5,18 @@ use cookie_factory::{
     gen_simple, GenError,
 };
 use core::mem;
+use nom::{
+    bytes::complete::take,
+    combinator::{map, map_res, verify},
+    error::ErrorKind,
+    IResult,
+};
 use packed_struct::{prelude::*, types::bits::Bits, PackedStructInfo};
 use smoltcp::wire::EthernetFrame;
 
 // TODO: Logical PDU with 32 bit address
 // TODO: Auto increment PDU with i16 address
+#[derive(Debug)]
 pub struct Pdu<const N: usize> {
     command: Command,
     index: u8,
@@ -25,7 +32,8 @@ impl<const N: usize> Pdu<N> {
         debug_assert!(N < LEN_MASK as usize);
 
         Self {
-            command: Command::Brd,
+            // Start at master address 0
+            command: Command::Brd { address: 0 },
             index: 0,
             register_address,
             flags: PduFlags::with_len(N as u16),
@@ -74,8 +82,56 @@ impl<const N: usize> Pdu<N> {
 
         Ok(buf)
     }
+
+    pub fn from_ethercat_frame<'a>(&self, i: &'a [u8]) -> IResult<&'a [u8], Option<Self>> {
+        let (i, _header) = FrameHeader::parse_pdu(i)?;
+
+        dbg!(_header.len());
+        dbg!(_header.protocol_type());
+
+        let (i, command_code) = map_res(nom::number::complete::u8, CommandCode::try_from)(i)?;
+
+        let (i, index) = nom::number::complete::u8(i)?;
+
+        // Possibly valid, but it's not our response
+        if index != self.index {
+            return Ok((i, None));
+        }
+
+        let (i, command) = command_code.parse_address(i)?;
+
+        dbg!(&self.command, &command);
+
+        // Check this is valid response to what we sent based on index
+        if !self.command.is_valid_response(&command) {
+            return Err(nom::Err::Error(nom::error::Error::new(
+                i,
+                ErrorKind::Verify,
+            )));
+        }
+
+        let (i, register_address) = nom::number::complete::le_u16(i)?;
+        let (i, flags) = map_res(take(2usize), PduFlags::unpack_from_slice)(i)?;
+        let (i, irq) = nom::number::complete::le_u16(i)?;
+        let (i, data) = map_res(take(N), |slice: &[u8]| slice.try_into())(i)?;
+        let (i, working_counter) = nom::number::complete::le_u16(i)?;
+
+        Ok((
+            i,
+            Some(Self {
+                command,
+                index,
+                register_address,
+                flags,
+                irq,
+                data,
+                working_counter,
+            }),
+        ))
+    }
 }
 
+#[derive(PartialEq, Debug)]
 enum Command {
     Aprd {
         /// Auto increment counter.
@@ -85,7 +141,10 @@ enum Command {
         /// Configured station address.
         address: u16,
     },
-    Brd,
+    Brd {
+        /// Autoincremented by each slave visted.
+        address: u16,
+    },
     Lrd {
         /// Logical address.
         address: u32,
@@ -97,7 +156,7 @@ impl Command {
         match self {
             Self::Aprd { .. } => CommandCode::Aprd,
             Self::Fprd { .. } => CommandCode::Fprd,
-            Self::Brd => CommandCode::Brd,
+            Self::Brd { .. } => CommandCode::Brd,
             Self::Lrd { .. } => CommandCode::Lrd,
         }
     }
@@ -107,12 +166,23 @@ impl Command {
 
         let buf = match self {
             Command::Aprd { address } => gen_simple(le_i16(*address), buf)?,
-            Command::Fprd { address } => gen_simple(le_u16(*address), buf)?,
+            Command::Fprd { address } | Command::Brd { address } => {
+                gen_simple(le_u16(*address), buf)?
+            }
             Command::Lrd { address } => gen_simple(le_u32(*address), buf)?,
-            Command::Brd => buf,
         };
 
         Ok(buf)
+    }
+
+    fn is_valid_response(&self, other: &Self) -> bool {
+        match self {
+            // Ignore addresses for autoincrement services; the master sends zero and any slave
+            // response is non-zero.
+            Command::Aprd { .. } | Command::Brd { .. } => self.code() == other.code(),
+            Command::Fprd { .. } => self == other,
+            Command::Lrd { .. } => self == other,
+        }
     }
 }
 
@@ -123,6 +193,40 @@ pub enum CommandCode {
     Fprd = 0x04,
     Brd = 0x07,
     Lrd = 0x0A,
+}
+
+impl CommandCode {
+    /// Parse an address, producing a [`Command`].
+    fn parse_address(self, i: &[u8]) -> IResult<&[u8], Command> {
+        match self {
+            Self::Aprd => map(nom::number::complete::le_i16, |address| Command::Aprd {
+                address,
+            })(i),
+            Self::Fprd => map(nom::number::complete::le_u16, |address| Command::Fprd {
+                address,
+            })(i),
+            Self::Brd => map(nom::number::complete::le_u16, |address| Command::Brd {
+                address,
+            })(i),
+            Self::Lrd => map(nom::number::complete::le_u32, |address| Command::Lrd {
+                address,
+            })(i),
+        }
+    }
+}
+
+impl TryFrom<u8> for CommandCode {
+    type Error = ();
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0x01 => Ok(Self::Aprd),
+            0x04 => Ok(Self::Fprd),
+            0x07 => Ok(Self::Brd),
+            0x0A => Ok(Self::Lrd),
+            _ => Err(()),
+        }
+    }
 }
 
 #[derive(Copy, Clone, Debug, PackedStruct, PartialEq)]
@@ -170,6 +274,26 @@ impl FrameHeader {
         let protocol_type = 0x01 << 12;
 
         Self(len | protocol_type)
+    }
+
+    fn parse_pdu(i: &[u8]) -> IResult<&[u8], Self> {
+        let (i, raw) = nom::number::complete::le_u16(i)?;
+
+        let len = raw & LEN_MASK;
+
+        // TODO: Take LEN_MASK + reserved bit and see if length has overflowed?
+
+        let self_ = Self(raw);
+
+        // TODO: Const
+        if self_.protocol_type() == 0x01 {
+            Ok((i, self_))
+        } else {
+            Err(nom::Err::Error(nom::error::Error::new(
+                i,
+                ErrorKind::Verify,
+            )))
+        }
     }
 
     fn len(&self) -> u16 {
