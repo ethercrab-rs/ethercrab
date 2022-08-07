672bcfb2f66d033be660c38f939844f2afa9d91b
James Waples <james@wapl.es>
2022-08-07 13:17:20 +0100

    TODOs

--- src/pdu_loop/mod.rs
+++ src/pdu_loop/mod.rs
@@ -81,6 +81,7 @@ where
         &self,
         command: Command,
         data: &[u8],
+        // TODO: Send zeroes when reading instead of a length
         data_length: u16,
     ) -> Result<Pdu<MAX_PDU_DATA>, PduError> {
         let idx = self.idx.fetch_add(1, Ordering::AcqRel) % MAX_FRAMES as u8;
