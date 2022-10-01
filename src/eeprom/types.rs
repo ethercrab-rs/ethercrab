fa6ed2530553c7d112819afc784de86bb4ff4107
James Waples <james@wapl.es>
2022-10-01 13:06:47 +0100

    WIP mailbox support

--- src/eeprom/types.rs
+++ src/eeprom/types.rs
@@ -4,7 +4,6 @@ use core::fmt;
 
 use crate::{
     base_data_types::PrimitiveDataType,
-    mailbox::MailboxProtocols,
     sync_manager_channel::{self},
     PduRead,
 };
@@ -644,6 +643,26 @@ bitflags::bitflags! {
         const PDO_DIS_AUTO_EXCLUDE = 0x4000;
         /// Reserved (PdoWritable)
         const PDO_WRITABLE = 0x8000;
+    }
+}
+
+bitflags::bitflags! {
+    /// Supported mailbox category.
+    ///
+    /// Defined in ETG1000.6 Table 18 or ETG2010 Table 4.
+    pub struct MailboxProtocols: u16 {
+        /// ADS over EtherCAT (routing and parallel services).
+        const AOE = 0x0001;
+        /// Ethernet over EtherCAT (tunnelling of Data Link services).
+        const EOE = 0x0002;
+        /// CAN application protocol over EtherCAT (access to SDO).
+        const COE = 0x0004;
+        /// File Access over EtherCAT.
+        const FOE = 0x0008;
+        /// Servo Drive Profile over EtherCAT.
+        const SOE = 0x0010;
+        /// Vendor specific protocol over EtherCAT.
+        const VOE = 0x0020;
     }
 }
 
