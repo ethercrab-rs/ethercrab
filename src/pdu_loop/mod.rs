bd624780c462b8ce0438ba024f10ed89980b56c8
James Waples <james@wapl.es>
2022-10-05 10:36:07 +0100

    WIP/broken data references instead of moves

--- src/pdu_loop/mod.rs
+++ src/pdu_loop/mod.rs
@@ -38,10 +38,10 @@ impl<T> CheckWorkingCounter<T> for PduResponse<T> {
     }
 }
 
-pub struct PduLoop<const MAX_FRAMES: usize, const MAX_PDU_DATA: usize, TIMEOUT> {
+pub struct PduLoop<'a, const MAX_FRAMES: usize, TIMEOUT> {
     // TODO: Can we have a single buffer that gives out variable length slices instead of wasting
     // space with lots of potentially huge PDUs?
-    frames: [UnsafeCell<pdu_frame::Frame<MAX_PDU_DATA>>; MAX_FRAMES],
+    frames: [UnsafeCell<pdu_frame::Frame<'a>>; MAX_FRAMES],
     /// A waker used to wake up the TX task when a new frame is ready to be sent.
     tx_waker: RefCell<Option<Waker>>,
     /// EtherCAT frame index.
@@ -52,13 +52,9 @@ pub struct PduLoop<const MAX_FRAMES: usize, const MAX_PDU_DATA: usize, TIMEOUT> 
 // If we don't impl Send, does this guarantee we can have a PduLoopRef and not invalidate the
 // pointer? BBQueue does this.
 // TODO: Allow static init of `PduLoop` so it can be given to multiple threads if the user desires.
-unsafe impl<const MAX_FRAMES: usize, const MAX_PDU_DATA: usize, TIMEOUT> Sync
-    for PduLoop<MAX_FRAMES, MAX_PDU_DATA, TIMEOUT>
-{
-}
+unsafe impl<'a, const MAX_FRAMES: usize, TIMEOUT> Sync for PduLoop<'a, MAX_FRAMES, TIMEOUT> {}
 
-impl<const MAX_FRAMES: usize, const MAX_PDU_DATA: usize, TIMEOUT>
-    PduLoop<MAX_FRAMES, MAX_PDU_DATA, TIMEOUT>
+impl<'a, const MAX_FRAMES: usize, TIMEOUT> PduLoop<'a, MAX_FRAMES, TIMEOUT>
 where
     TIMEOUT: TimerFactory,
 {
@@ -84,7 +80,7 @@ where
 
     pub fn send_frames_blocking<F>(&self, waker: &Waker, mut send: F) -> Result<(), ()>
     where
-        F: FnMut(&Pdu<MAX_PDU_DATA>) -> Result<(), ()>,
+        F: FnMut(&Pdu<'a>) -> Result<(), ()>,
     {
         self.frames.iter().try_for_each(|frame| {
             let frame = unsafe { &mut *frame.get() };
@@ -103,7 +99,7 @@ where
         Ok(())
     }
 
-    fn frame(&self, idx: u8) -> Result<&mut pdu_frame::Frame<MAX_PDU_DATA>, Error> {
+    fn frame(&self, idx: u8) -> Result<&mut pdu_frame::Frame<'a>, Error> {
         let req = self
             .frames
             .get(usize::from(idx))
@@ -117,12 +113,12 @@ where
         command: Command,
         data: &[u8],
         data_length: u16,
-    ) -> Result<Pdu<MAX_PDU_DATA>, Error> {
+    ) -> Result<Pdu<'a>, Error> {
         let idx = self.idx.fetch_add(1, Ordering::AcqRel) % MAX_FRAMES as u8;
 
         let frame = self.frame(idx)?;
 
-        let pdu = Pdu::<MAX_PDU_DATA>::new(command, data_length, idx, data)?;
+        let pdu = Pdu::new(command, data_length, idx, data)?;
 
         frame.replace(pdu)?;
 
@@ -152,7 +148,7 @@ where
 
         let frame = self.frame(idx)?;
 
-        let pdu = Pdu::<MAX_PDU_DATA>::new(command, read_length, idx, buffer)?;
+        let pdu = Pdu::new(command, read_length, idx, buffer)?;
 
         frame.replace(pdu)?;
 
@@ -186,10 +182,9 @@ where
             return Ok(());
         }
 
-        let (_rest, pdu) = Pdu::<MAX_PDU_DATA>::from_ethernet_payload::<nom::error::Error<&[u8]>>(
-            raw_packet.payload(),
-        )
-        .map_err(|_| PduError::Parse)?;
+        let (_rest, pdu) =
+            Pdu::from_ethernet_payload::<nom::error::Error<&[u8]>>(raw_packet.payload())
+                .map_err(|_| PduError::Parse)?;
 
         self.frame(pdu.index())?.wake_done(pdu)?;
 
