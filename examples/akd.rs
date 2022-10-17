67f1feca3324d96d4703695c6fc32646e6acda45
James Waples <james@wapl.es>
2022-10-17 17:49:34 +0100

    :tada: :tada: :tada: Enable AKD in position mode

---
+++ examples/akd.rs
@@ -0,0 +1,438 @@
+//! Configure a Kollmorgen AKD servo drive and put it in enabled state.
+
+use async_ctrlc::CtrlC;
+use async_io::Timer;
+use ethercrab::coe::SdoAccess;
+use ethercrab::error::Error;
+use ethercrab::std::tx_rx_task;
+use ethercrab::Client;
+use ethercrab::PduLoop;
+use ethercrab::SlaveGroup;
+use ethercrab::SlaveState;
+use futures_lite::FutureExt;
+use futures_lite::StreamExt;
+use smol::LocalExecutor;
+use std::sync::Arc;
+use std::time::Duration;
+
+#[cfg(target_os = "windows")]
+// ASRock NIC
+// const INTERFACE: &str = "TODO";
+// // USB NIC
+// const INTERFACE: &str = "\\Device\\NPF_{DCEDC919-0A20-47A2-9788-FC57D0169EDB}";
+// Lenovo USB-C NIC
+const INTERFACE: &str = "\\Device\\NPF_{CC0908D5-3CB8-46D6-B8A2-575D0578008D}";
+// Silver USB NIC
+// const INTERFACE: &str = "\\Device\\NPF_{CC0908D5-3CB8-46D6-B8A2-575D0578008D}";
+#[cfg(not(target_os = "windows"))]
+const INTERFACE: &str = "eth1";
+
+const MAX_SLAVES: usize = 16;
+const MAX_PDU_DATA: usize = 1100;
+const MAX_FRAMES: usize = 16;
+const PDI_LEN: usize = 64;
+
+static PDU_LOOP: PduLoop<MAX_FRAMES, MAX_PDU_DATA, smol::Timer> = PduLoop::new();
+
+async fn main_inner(ex: &LocalExecutor<'static>) -> Result<(), Error> {
+    log::info!("Starting SDO demo...");
+
+    let client = Arc::new(Client::<MAX_FRAMES, MAX_PDU_DATA, smol::Timer>::new(
+        &PDU_LOOP,
+    ));
+
+    ex.spawn(tx_rx_task(INTERFACE, &client).unwrap()).detach();
+
+    // let num_slaves = client.num_slaves();
+
+    let groups =
+        [SlaveGroup::<MAX_SLAVES, PDI_LEN, MAX_FRAMES, MAX_PDU_DATA, _>::new(Box::new(|slave| {
+            Box::pin(async {
+                // --- Reads ---
+
+                // // Name
+                // dbg!(slave
+                //     .read_sdo::<heapless::String<64>>(0x1008, SdoAccess::Index(0))
+                //     .await
+                //     .unwrap());
+
+                // // Software version. For AKD, this should equal "M_01-20-00-003"
+                // dbg!(slave
+                //     .read_sdo::<heapless::String<64>>(0x100a, SdoAccess::Index(0))
+                //     .await
+                //     .unwrap());
+
+                // --- Writes ---
+
+                log::info!("Found {}", slave.name());
+
+                // AKD config
+                if slave.name() == "AKD" {
+                    slave.write_sdo(0x1c12, 0u8, SdoAccess::Index(0)).await?;
+                    slave
+                        .write_sdo(0x1c12, 0x1701u16, SdoAccess::Index(1))
+                        .await?;
+                    slave.write_sdo(0x1c12, 0x01u8, SdoAccess::Index(0)).await?;
+
+                    // Must set both read AND write SDOs for AKD otherwise it times out going into OP
+                    slave.write_sdo(0x1c13, 0u8, SdoAccess::Index(0)).await?;
+                    slave
+                        .write_sdo(0x1c13, 0x1B01u16, SdoAccess::Index(1))
+                        .await?;
+                    slave.write_sdo(0x1c13, 0x01u8, SdoAccess::Index(0)).await?;
+
+                    // Opmode - Cyclic Synchronous Position
+                    //  slave.write_sdo(0x6060, 0x08, SdoAccess::Index(0)).await?;
+                    // Opmode - Cyclic Synchronous Velocity
+                    slave.write_sdo(0x6060, 0x09u8, SdoAccess::Index(0)).await?;
+
+                    // DELETEME: Read this later, after init
+                    {
+                        let base = slave.read_sdo::<u8>(0x60c2, SdoAccess::Index(1)).await?;
+                        let x10 = slave.read_sdo::<i8>(0x60c2, SdoAccess::Index(2)).await?;
+
+                        let base = f32::from(base);
+                        let x10 = 10.0f32.powi(i32::from(x10));
+
+                        let cycle_time_ms = (base * x10) * 1000.0;
+                        let cycle_time_ms = Duration::from_millis(unsafe {
+                            cycle_time_ms.round().to_int_unchecked()
+                        });
+
+                        log::debug!("Cycle time: {} ms", cycle_time_ms.as_millis());
+                    }
+                }
+
+                if slave.name() == "ELP-EC400S" {
+                    slave.write_sdo(0x1c12, 0u8, SdoAccess::Index(0)).await?;
+                    slave
+                        .write_sdo(0x1c12, 0x1601u16, SdoAccess::Index(1))
+                        .await?;
+                    slave.write_sdo(0x1c12, 0x01u8, SdoAccess::Index(0)).await?;
+
+                    slave.write_sdo(0x1c13, 0u8, SdoAccess::Index(0)).await?;
+                    slave
+                        .write_sdo(0x1c13, 0x1A00u16, SdoAccess::Index(1))
+                        .await?;
+                    slave.write_sdo(0x1c13, 0x01u8, SdoAccess::Index(0)).await?;
+                }
+
+                Ok(())
+            })
+        })); 1];
+
+    let mut groups = client
+        .init(groups, |groups, slave| {
+            // All slaves MUST end up in a group or they'll remain uninitialised
+            groups[0].push(slave).expect("Too many slaves");
+
+            // TODO: Return a group key so the user has to put the slave somewhere
+        })
+        .await
+        .expect("Init");
+
+    client
+        .request_slave_state(SlaveState::Op)
+        .await
+        .expect("OP");
+
+    log::info!("Slaves moved to OP state");
+
+    let group = groups.get_mut(0).expect("No group!");
+
+    log::info!("Group has {} slaves", group.slaves().len());
+
+    // Run twice to prime PDI
+    group.tx_rx(&client).await.expect("TX/RX");
+
+    // TODO: API to get individual slave from group. Requires splitting up `SlaveRef` first.
+    // {
+    //     let slave = &group.slave(&client, 0).unwrap();
+
+    //     // let cycle_x10 =
+    // }
+
+    // TODO: Read from AKD 0x60c1
+    // AKD will error with F706 if cycle time is not 2ms or less
+    let mut cyclic_interval = Timer::interval(Duration::from_millis(2));
+
+    // Check for and clear faults
+    {
+        log::info!("Checking faults");
+
+        group.tx_rx(&client).await.expect("TX/RX");
+
+        let (i, o) = group.io(0).unwrap();
+
+        let status = i
+            .map(|i| {
+                let status = u16::from_le_bytes(i[4..=5].try_into().unwrap());
+
+                unsafe { AkdStatusWord::from_bits_unchecked(status) }
+            })
+            .unwrap();
+
+        if status.contains(AkdStatusWord::FAULT) {
+            log::warn!("Fault! Clearing...");
+
+            o.map(|o| {
+                let (_pos_cmd, control) = o.split_at_mut(4);
+                let reset = AkdControlWord::RESET_FAULT;
+                let reset = reset.bits().to_le_bytes();
+                control.copy_from_slice(&reset);
+            });
+
+            while let Some(_) = cyclic_interval.next().await {
+                group.tx_rx(&client).await.expect("TX/RX");
+
+                let (i, _o) = group.io(0).unwrap();
+
+                let status = i
+                    .map(|i| {
+                        let status = u16::from_le_bytes(i[4..=5].try_into().unwrap());
+
+                        unsafe { AkdStatusWord::from_bits_unchecked(status) }
+                    })
+                    .unwrap();
+
+                if !status.contains(AkdStatusWord::FAULT) {
+                    log::info!("Fault cleared, status is now {status:?}");
+
+                    break;
+                }
+            }
+        }
+    }
+
+    // Shutdown state
+    {
+        log::info!("Putting drive in shutdown state");
+
+        let (_i, o) = group.io(0).unwrap();
+
+        o.map(|o| {
+            let (_pos_cmd, control) = o.split_at_mut(4);
+            let value = AkdControlWord::SHUTDOWN;
+            let value = value.bits().to_le_bytes();
+            control.copy_from_slice(&value);
+        });
+
+        while let Some(_) = cyclic_interval.next().await {
+            group.tx_rx(&client).await.expect("TX/RX");
+
+            let (i, _o) = group.io(0).unwrap();
+
+            let status = i
+                .map(|i| {
+                    let status = u16::from_le_bytes(i[4..=5].try_into().unwrap());
+
+                    unsafe { AkdStatusWord::from_bits_unchecked(status) }
+                })
+                .unwrap();
+
+            dbg!(status);
+
+            if status.contains(AkdStatusWord::READY_TO_SWITCH_ON) {
+                log::info!("Drive is shut down");
+
+                break;
+            }
+        }
+    }
+
+    // Switch drive on
+    {
+        log::info!("Switching drive on");
+
+        let (_i, o) = group.io(0).unwrap();
+
+        o.map(|o| {
+            let (_pos_cmd, control) = o.split_at_mut(4);
+            let reset = AkdControlWord::SWITCH_ON
+                | AkdControlWord::DISABLE_VOLTAGE
+                | AkdControlWord::QUICK_STOP;
+            let reset = reset.bits().to_le_bytes();
+            control.copy_from_slice(&reset);
+        });
+
+        while let Some(_) = cyclic_interval.next().await {
+            group.tx_rx(&client).await.expect("TX/RX");
+
+            let (i, o) = group.io(0).unwrap();
+
+            let status = i
+                .map(|i| {
+                    let status = u16::from_le_bytes(i[4..=5].try_into().unwrap());
+
+                    unsafe { AkdStatusWord::from_bits_unchecked(status) }
+                })
+                .unwrap();
+
+            if status.contains(AkdStatusWord::SWITCHED_ON) {
+                log::info!("Drive switched on, begin cyclic operation");
+
+                o.map(|o| {
+                    let (_pos_cmd, control) = o.split_at_mut(4);
+
+                    // Enable operation so we can send cyclic data
+                    let state = AkdControlWord::SWITCH_ON
+                        | AkdControlWord::DISABLE_VOLTAGE
+                        | AkdControlWord::QUICK_STOP
+                        | AkdControlWord::ENABLE_OP;
+                    let state = state.bits().to_le_bytes();
+                    control.copy_from_slice(&state);
+                });
+
+                break;
+            }
+        }
+    }
+
+    // // Enable operation
+    // {
+    //     let (_i, o) = group.io(0).unwrap();
+
+    //     o.map(|o| {
+    //         let (_pos_cmd, control) = o.split_at_mut(4);
+    //         let reset = AkdControlWord::SWITCH_ON
+    //             | AkdControlWord::DISABLE_VOLTAGE
+    //             | AkdControlWord::QUICK_STOP;
+    //         let reset = reset.bits().to_le_bytes();
+    //         control.copy_from_slice(&reset);
+    //     });
+
+    //     while let Some(_) = cyclic_interval.next().await {
+    //         group.tx_rx(&client).await.expect("TX/RX");
+
+    //         let (i, _o) = group.io(0).unwrap();
+
+    //         let status = i
+    //             .map(|i| {
+    //                 let status = u16::from_le_bytes(i[4..=5].try_into().unwrap());
+
+    //                 unsafe { AkdStatusWord::from_bits_unchecked(status) }
+    //             })
+    //             .unwrap();
+
+    //         if !status.contains(AkdStatusWord::OP_ENABLED) {
+    //             log::info!("Drive enabled");
+
+    //             break;
+    //         }
+    //     }
+    // }
+
+    while let Some(_) = cyclic_interval.next().await {
+        group.tx_rx(&client).await.expect("TX/RX");
+
+        let (i, o) = group.io(0).unwrap();
+
+        let (pos, status) = i
+            .map(|i| {
+                let pos = u32::from_le_bytes(i[0..=3].try_into().unwrap());
+                let status = u16::from_le_bytes(i[4..=5].try_into().unwrap());
+
+                let status = unsafe { AkdStatusWord::from_bits_unchecked(status) };
+
+                (pos, status)
+            })
+            .unwrap();
+
+        println!(
+            "Position: {pos}, Status: {status:?} | {:?}",
+            o.as_ref().unwrap()
+        );
+
+        o.map(|o| {
+            let (pos_cmd, _control) = o.split_at_mut(4);
+
+            pos_cmd.copy_from_slice(&pos.to_le_bytes());
+
+            // let state = AkdControlWord::SWITCH_ON
+            //     | AkdControlWord::DISABLE_VOLTAGE
+            //     | AkdControlWord::QUICK_STOP
+            //     | AkdControlWord::ENABLE_OP;
+            // let state = state.bits().to_le_bytes();
+            // control.copy_from_slice(&state);
+        });
+    }
+
+    Ok(())
+}
+
+bitflags::bitflags! {
+    /// AKD EtherCAT Communications Manual section 5.3.55
+    struct AkdControlWord: u16 {
+        /// Switch on
+        const SWITCH_ON = 1 << 0;
+        /// Disable Voltage
+        const DISABLE_VOLTAGE = 1 << 1;
+        /// Quick Stop
+        const QUICK_STOP = 1 << 2;
+        /// Enable Operation
+        const ENABLE_OP = 1 << 3;
+        /// Operation mode specific
+        const OP_SPECIFIC_1 = 1 << 4;
+        /// Operation mode specific
+        const OP_SPECIFIC_2 = 1 << 5;
+        /// Operation mode specific
+        const OP_SPECIFIC_3 = 1 << 6;
+        /// Reset Fault (only effective for faults)
+        const RESET_FAULT = 1 << 7;
+        /// Pause/halt
+        const PAUSE = 1 << 8;
+
+        const SHUTDOWN = Self::DISABLE_VOLTAGE.bits | Self::QUICK_STOP.bits;
+    }
+}
+
+bitflags::bitflags! {
+    /// AKD EtherCAT Communications Manual section   5.3.56
+    struct AkdStatusWord: u16 {
+        /// Ready to switch on
+        const READY_TO_SWITCH_ON = 1 << 0;
+        /// Switched on
+        const SWITCHED_ON = 1 << 1;
+        /// Operation enabled
+        const OP_ENABLED = 1 << 2;
+        /// Fault
+        const FAULT = 1 << 3;
+        /// Voltage enabled
+        const VOLTAGE_ENABLED = 1 << 4;
+        /// Quick stop
+        const QUICK_STOP = 1 << 5;
+        /// Switch on disabled
+        const SWITCH_ON_DISABLED = 1 << 6;
+        /// Warning
+        const WARNING = 1 << 7;
+        /// STO – Safe Torque Off
+        const STO = 1 << 8;
+        /// Remote
+        const REMOTE = 1 << 9;
+        /// Target reached
+        const TARGET_REACHED = 1 << 10;
+        /// Internal limit active
+        const INTERNAL_LIMIT = 1 << 11;
+        /// Operation mode specific (reserved)
+        const OP_SPECIFIC_1 = 1 << 12;
+        /// Operation mode specific (reserved)
+        const OP_SPECIFIC_2 = 1 << 13;
+        /// Manufacturer-specific (reserved)
+        const MAN_SPECIFIC_1 = 1 << 14;
+        /// Manufacturer-specific (reserved)
+        const MAN_SPECIFIC_2 = 1 << 15;
+    }
+}
+
+fn main() -> Result<(), Error> {
+    env_logger::init();
+    let local_ex = LocalExecutor::new();
+
+    let ctrlc = CtrlC::new().expect("cannot create Ctrl+C handler?");
+
+    futures_lite::future::block_on(
+        local_ex.run(ctrlc.race(async { main_inner(&local_ex).await.unwrap() })),
+    );
+
+    Ok(())
+}
