ecc8c6e73ce9fd2d9bec8cfe367fc5e8762a295a
James Waples <james@wapl.es>
2023-01-07 23:00:50 +0000

    Add more comments

--- src/slave_group/container.rs
+++ src/slave_group/container.rs
@@ -1,12 +1,19 @@
 use super::SlaveGroupRef;
 use crate::SlaveGroup;
 
+/// This trait must be implemented for the item passed to
+/// [`Client::init`](crate::client::Client::init).
+///
+/// For convenience, this trait is already implemented for [`SlaveGroup`] for single-group use
+/// cases, as well as `[SlaveGroup; N]` for simple, multi-group uses.
 pub trait SlaveGroupContainer<TIMEOUT> {
+    /// The number of slave groups in the container.
     fn num_groups(&self) -> usize;
 
     /// Get a group by index.
     fn group(&mut self, index: usize) -> Option<SlaveGroupRef<TIMEOUT>>;
 
+    /// Count the total number of slave devices held across all groups in this container.
     fn total_slaves(&mut self) -> usize {
         let mut accum = 0;
 
