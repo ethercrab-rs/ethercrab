c2fcc1767860447ee38610ec6f727910c6c7812f
James Waples <james@wapl.es>
2023-01-14 18:02:32 +0000

    Refactor EEPROM internals (#8)
    - Add `FromEeprom` trait to hold read size of structs instead of having magic numbers everywhere
    - Clean up EepromReader API to make it clearer to use
    - Refactor `EepromReader` to make it nicer

--- src/eeprom/reader.rs
+++ src/eeprom/reader.rs
@@ -1,8 +1,14 @@
 use crate::{
-    eeprom::{types::SiiCategory, Eeprom},
+    eeprom::types::{CategoryType, SiiControl, SiiReadSize, SiiRequest},
     error::{EepromError, Error},
+    register::RegisterAddress,
+    slave::slave_client::SlaveClient,
     timer_factory::TimerFactory,
 };
+
+/// The address of the first proper category, positioned after the fixed fields defined in ETG2010
+/// Table 2.
+const SII_FIRST_CATEGORY_START: u16 = 0x0040u16;
 
 /// EEPROM section reader.
 ///
@@ -13,13 +19,14 @@ pub struct EepromSectionReader<'a, TIMEOUT> {
     len: u16,
     byte_count: u16,
     read: heapless::Deque<u8, 8>,
-    eeprom: &'a Eeprom<'a, TIMEOUT>,
+    // eeprom: &'a Eeprom<'a, TIMEOUT>,
     read_length: usize,
+    client: &'a SlaveClient<'a, TIMEOUT>,
 }
 
 impl<'a, TIMEOUT> EepromSectionReader<'a, TIMEOUT>
 where
-    TIMEOUT: TimerFactory,
+    TIMEOUT: TimerFactory + 'a,
 {
     /// Create a new EEPROM section reader.
     ///
@@ -27,21 +34,151 @@ where
     /// `EepromSectionReader` will either return [`EepromError::SectionOverrun`] or
     /// [`EepromError::SectionUnderrun`] errors if the section cannot be completely read as this is
     /// often an indicator of a bug in either the slave's EEPROM or EtherCrab.
-    pub fn new(eeprom: &'a Eeprom<'a, TIMEOUT>, cat: SiiCategory) -> Self {
-        Self::start_at(eeprom, cat.start, cat.len_words * 2)
+    pub async fn new(
+        client: &'a SlaveClient<'a, TIMEOUT>,
+        category: CategoryType,
+    ) -> Result<Option<EepromSectionReader<'_, TIMEOUT>>, Error> {
+        let mut start_word = SII_FIRST_CATEGORY_START;
+
+        loop {
+            let chunk = Self::read_eeprom_raw(client, start_word).await?;
+
+            let category_type =
+                CategoryType::from(u16::from_le_bytes(chunk[0..2].try_into().unwrap()));
+            let len_words = u16::from_le_bytes(chunk[2..4].try_into().unwrap());
+
+            // Position after header
+            start_word += 2;
+
+            log::trace!(
+                "Found category {category_type:?}, data starts at {:#06x?}, length {:#04x?} ({}) bytes",
+                start_word,
+                len_words,
+                len_words
+            );
+
+            match category_type {
+                cat if cat == category => {
+                    break Ok(Some(Self::start_at(client, start_word, len_words * 2)));
+                }
+                CategoryType::End => break Ok(None),
+                _ => (),
+            }
+
+            // Next category starts after the current category's data
+            start_word += len_words;
+        }
     }
 
     /// Read an arbitrary chunk of the EEPROM instead of using an EEPROM section configu to define
     /// start address and length.
-    pub fn start_at(eeprom: &'a Eeprom<'a, TIMEOUT>, address: u16, len_bytes: u16) -> Self {
+    pub fn start_at(client: &'a SlaveClient<'a, TIMEOUT>, address: u16, len_bytes: u16) -> Self {
         Self {
-            eeprom,
             start: address,
             len: len_bytes,
             byte_count: 0,
             read: heapless::Deque::new(),
             read_length: 0,
+            client,
         }
+    }
+
+    async fn read_eeprom_raw<'client>(
+        client: &'client SlaveClient<'client, TIMEOUT>,
+        eeprom_address: u16,
+    ) -> Result<[u8; 8], Error> {
+        let status = client
+            .read::<SiiControl>(RegisterAddress::SiiControl, "Read SII control")
+            .await?;
+
+        // Clear errors
+        if status.has_error() {
+            log::trace!("Resetting EEPROM error flags");
+
+            client
+                .write(
+                    RegisterAddress::SiiControl,
+                    status.error_reset().as_array(),
+                    "Reset errors",
+                )
+                .await?;
+        }
+
+        // Set up an SII read. This writes the control word and the register word after it
+        // TODO: Consider either removing context strings or using defmt or something to avoid
+        // bloat.
+        client
+            .write(
+                RegisterAddress::SiiControl,
+                SiiRequest::read(eeprom_address).as_array(),
+                "SII read setup",
+            )
+            .await?;
+
+        Self::wait(client).await?;
+
+        let data = match status.read_size {
+            // If slave uses 4 octet reads, do two reads so we can always return a chunk of 8 bytes
+            SiiReadSize::Octets4 => {
+                let chunk1 = client
+                    .read::<[u8; 4]>(RegisterAddress::SiiData, "Read SII data")
+                    .await?;
+
+                // Move on to next chunk
+                {
+                    // NOTE: We must compute offset in 16 bit words, not bytes, hence the divide by 2
+                    let setup = SiiRequest::read(eeprom_address + (chunk1.len() / 2) as u16);
+
+                    client
+                        .write(
+                            RegisterAddress::SiiControl,
+                            setup.as_array(),
+                            "SII read setup",
+                        )
+                        .await?;
+
+                    Self::wait(client).await?;
+                }
+
+                let chunk2 = client
+                    .read::<[u8; 4]>(RegisterAddress::SiiData, "SII data 2")
+                    .await?;
+
+                let mut data = [0u8; 8];
+
+                data[0..4].copy_from_slice(&chunk1);
+                data[4..8].copy_from_slice(&chunk2);
+
+                data
+            }
+            SiiReadSize::Octets8 => {
+                client
+                    .read::<[u8; 8]>(RegisterAddress::SiiData, "SII data")
+                    .await?
+            }
+        };
+
+        log::trace!("Read {:#04x?} {:02x?}", eeprom_address, data);
+
+        Ok(data)
+    }
+
+    /// Wait for EEPROM read or write operation to finish and clear the busy flag.
+    async fn wait<'client>(client: &'client SlaveClient<'client, TIMEOUT>) -> Result<(), Error> {
+        crate::timer_factory::timeout::<TIMEOUT, _, _>(client.timeouts().eeprom, async {
+            loop {
+                let control = client
+                    .read::<SiiControl>(RegisterAddress::SiiControl, "SII busy wait")
+                    .await?;
+
+                if !control.busy {
+                    break Ok(());
+                }
+
+                client.timeouts().loop_tick::<TIMEOUT>().await;
+            }
+        })
+        .await
     }
 
     /// Read the next byte from the EEPROM.
@@ -49,7 +186,7 @@ where
     /// Internally, this method reads the EEPROM in chunks of 4 or 8 bytes (depending on the slave).
     pub async fn next(&mut self) -> Result<Option<u8>, Error> {
         if self.read.is_empty() {
-            let read = self.eeprom.read_eeprom_raw(self.start).await?;
+            let read = Self::read_eeprom_raw(self.client, self.start).await?;
 
             let slice = read.as_slice();
 
@@ -98,34 +235,37 @@ where
         }
     }
 
-    /// Read up to `N` bytes from the EEPROM.
-    pub async fn take_vec<const N: usize>(
-        &mut self,
-    ) -> Result<Option<heapless::Vec<u8, N>>, Error> {
-        self.take_n_vec(N).await
-    }
-
-    /// Attempt to exactly fill a buffer with bytes read from the EEPROM.
-    ///
-    /// If the current section under- or over-fills the buffer, an error is returned.
+    /// Attempt to read exactly `N` bytes. If not enough data could be read, this method returns an
+    /// error.
     pub async fn take_vec_exact<const N: usize>(&mut self) -> Result<heapless::Vec<u8, N>, Error> {
-        self.take_n_vec(N)
+        self.take_vec()
             .await?
             .ok_or(Error::Eeprom(EepromError::SectionUnderrun))
     }
 
-    /// Attempt to take an exact number of bytes.
-    pub async fn take_n_vec_exact<const N: usize>(
+    /// Read up to `N` bytes. If not enough data could be read, this method will return `Ok(None)`.
+    pub async fn take_vec<const N: usize>(
+        &mut self,
+    ) -> Result<Option<heapless::Vec<u8, N>>, Error> {
+        self.take_vec_len(N).await
+    }
+
+    /// Try to take `len` bytes, returning an error if the buffer length `N` is too small.
+    ///
+    /// If not enough data could be read, this method returns an error.
+    pub async fn take_vec_len_exact<const N: usize>(
         &mut self,
         len: usize,
     ) -> Result<heapless::Vec<u8, N>, Error> {
-        self.take_n_vec(len)
+        self.take_vec_len(len)
             .await?
             .ok_or(Error::Eeprom(EepromError::SectionUnderrun))
     }
 
     /// Try to take `len` bytes, returning an error if the buffer length `N` is too small.
-    pub async fn take_n_vec<const N: usize>(
+    ///
+    /// If not enough data can be read to fill the buffer, this method will return `Ok(None)`.
+    pub async fn take_vec_len<const N: usize>(
         &mut self,
         len: usize,
     ) -> Result<Option<heapless::Vec<u8, N>>, Error> {
@@ -134,7 +274,7 @@ where
         let mut count = 0;
 
         log::trace!(
-            "Taking bytes from EEPROM start {}, len {}, N {}",
+            "Taking bytes from EEPROM start {:#06x?}, len {}, N {}",
             self.start,
             len,
             N
