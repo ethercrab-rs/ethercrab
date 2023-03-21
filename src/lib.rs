782b31ba1e39cdcca96ac68835e0637919524fec
James Waples <james@wapl.es>
2023-03-21 19:15:31 +0000

    Better TX/RX in Linux (#33)
    * Use a custom implementation of a raw file descriptor in unix
    
    * Log errors with context for timeouts
    
    * Return better error types from TX/RX

--- src/lib.rs
+++ src/lib.rs
@@ -33,12 +33,12 @@
 //!
 //! ```rust,no_run
 //! use async_ctrlc::CtrlC;
-//! use async_io::Timer;
 //! use ethercrab::{
 //!     error::Error, std::tx_rx_task, Client, PduLoop, PduStorage, SlaveGroup, SubIndex, Timeouts,
 //!     ClientConfig
 //! };
 //! use futures_lite::{FutureExt, StreamExt};
+//! use smol::Timer;
 //! use smol::LocalExecutor;
 //! use std::{sync::Arc, time::Duration};
 //!
@@ -210,7 +210,8 @@ pub use slave_state::SlaveState;
 pub use timer_factory::Timeouts;
 
 const LEN_MASK: u16 = 0b0000_0111_1111_1111;
-const ETHERCAT_ETHERTYPE: EthernetProtocol = EthernetProtocol::Unknown(0x88a4);
+const ETHERCAT_ETHERTYPE_RAW: u16 = 0x88a4;
+const ETHERCAT_ETHERTYPE: EthernetProtocol = EthernetProtocol::Unknown(ETHERCAT_ETHERTYPE_RAW);
 const MASTER_ADDR: EthernetAddress = EthernetAddress([0x10, 0x10, 0x10, 0x10, 0x10, 0x10]);
 
 /// Starting address for discovered slaves.
