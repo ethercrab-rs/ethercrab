7ee75f1b06099f17c19d9d9437917ff562be057f
Jimy Byerley <jimy.byerley@gmail.com>
2023-05-09 10:06:09 +0200

    adapted convenience structs to changes from master

--- src/convenience/sdo.rs
+++ src/convenience/sdo.rs
@@ -1,9 +1,9 @@
 use crate::{
-	SlaveRef, SubIndex, PduData,
+	Slave, SlaveRef, SubIndex, PduData,
 	error::Error,
 	};
 use super::field::{Field, DType};
-use std::fmt;
+use std::{fmt, borrow::Borrow};
 
 /// description of an SDO's subitem, not a SDO itself
 #[derive(Clone)]
@@ -18,12 +18,12 @@ pub struct SubItem<T: PduData + DType> {
 }
 impl<T: PduData + DType> SubItem<T> {
 	/// retreive the current subitem value from the given slave
-	pub async fn get<'a>(&self, slave: &SlaveRef<'a>) -> Result<T, Error>  {
-		slave.read_sdo(self.index, SubIndex::Index(self.sub)).await
+	pub async fn get<'a, S: Borrow<Slave>>(&self, slave: &SlaveRef<'a, S>) -> Result<T, Error>  {
+		slave.sdo_read(self.index, SubIndex::Index(self.sub)).await
 	}
 	/// set the subitem value on the given slave
-	pub async fn set<'a>(&self, slave: &SlaveRef<'a>, value: T) -> Result<(), Error>   {
-		slave.write_sdo(self.index, SubIndex::Index(self.sub), value).await?;
+	pub async fn set<'a, S: Borrow<Slave>>(&self, slave: &SlaveRef<'a, S>, value: T) -> Result<(), Error>   {
+		slave.sdo_write(self.index, SubIndex::Index(self.sub), value).await?;
 		Ok(())
 	}
 }
@@ -62,41 +62,41 @@ impl fmt::Debug for SyncManager {
 
 
 /// configure a sync manager and select the pdos it is using
-pub struct SyncMapping<'a> {
-	slave: &'a SlaveRef<'a>,
+pub struct SyncMapping<'a, S> {
+	slave: &'a SlaveRef<'a, S>,
 	manager: &'a SyncManager,
 	offset: usize,
 	num: u8,
 }
 // unsafe impl<'a> Send for SyncMapping<'a> {}
 // unsafe impl<'a> Send for *mut SyncMapping<'a> {}
-impl<'a> SyncMapping<'a> {
-	pub async fn new(slave: &'a SlaveRef<'a>, manager: &'a SyncManager) -> Result<SyncMapping<'a>, Error> {
+impl<'a, S: Borrow<Slave>> SyncMapping<'a, S> {
+	pub async fn new(slave: &'a SlaveRef<'a, S>, manager: &'a SyncManager) -> Result<SyncMapping<'a, S>, Error> {
 		let new = Self {  
 			slave, 
 			manager, 
 			offset: 0,
 			num: 0,
 			};
-		new.slave.write_sdo(new.manager.index, SubIndex::Index(0), new.num).await?;
+		new.slave.sdo_write(new.manager.index, SubIndex::Index(0), new.num).await?;
 		Ok(new)
 	}
 	/// set the given pdo to be transmitted
-	pub async fn push<'b>(&'b mut self, pdo: &'b ConfigurablePdo) -> Result<PdoMapping<'b>, Error>
+	pub async fn push<'b>(&'b mut self, pdo: &'b ConfigurablePdo) -> Result<PdoMapping<'b, S>, Error>
 	where 'a: 'b {
 		assert!(self.num < self.manager.num, "maximum number of pdos reached");
 		log::debug!("push  {:x} {}: {:x}", self.manager.index, self.num+1, pdo.index);
-		self.slave.write_sdo(self.manager.index, SubIndex::Index(self.num+1), pdo.index).await?;
+		self.slave.sdo_write(self.manager.index, SubIndex::Index(self.num+1), pdo.index).await?;
 		self.num += 1;
 		
 		// this savage cast is here to prevent rust asking 'b to outlive 'a, which is non-sense
 		// the cast is in a separate function because the compiler seems to keep the pointer value alive during the following await, out of the unsafe block, which cause the async function to be send
-		fn cast<'a, 'b>(value: &'b mut SyncMapping<'a>) -> &'b mut SyncMapping<'b>
+		fn cast<'a, 'b, S>(value: &'b mut SyncMapping<'a, S>) -> &'b mut SyncMapping<'b, S>
 		where 'a: 'b {
 			unsafe {
 				&mut *std::mem::transmute::<
-							*mut SyncMapping<'a>, 
-							*mut SyncMapping<'b>,
+							*mut SyncMapping<'a, S>, 
+							*mut SyncMapping<'b, S>,
 							>(value)
 		}}
 		Ok(PdoMapping::new(cast(self), pdo).await?) 
@@ -105,22 +105,22 @@ impl<'a> SyncMapping<'a> {
 	/// must be called or the sync manager will not transmit
 	pub async fn finish(&mut self) -> Result<(), Error> {
 		log::debug!("finish  {:x} {}: {:x}", self.manager.index, 0, self.num);
-		self.slave.write_sdo(self.manager.index, SubIndex::Index(0), self.num).await?;
+		self.slave.sdo_write(self.manager.index, SubIndex::Index(0), self.num).await?;
 		Ok(())
 	}
 }
 
 
 /// configure a pdo and select the sdos subitems it is using
-pub struct PdoMapping<'b> {
-	mapping: &'b mut SyncMapping<'b>,
+pub struct PdoMapping<'b, S> {
+	mapping: &'b mut SyncMapping<'b, S>,
 	pdo: &'b ConfigurablePdo,
 	num: u8,
 }
-impl<'b> PdoMapping<'b> {
-	async fn new(mapping: &'b mut SyncMapping<'b>, pdo: &'b ConfigurablePdo) -> Result<PdoMapping<'b>, Error> {
+impl<'b, S: Borrow<Slave>> PdoMapping<'b, S> {
+	async fn new(mapping: &'b mut SyncMapping<'b, S>, pdo: &'b ConfigurablePdo) -> Result<PdoMapping<'b, S>, Error> {
 		let new = Self {mapping, pdo, num: 0};
-		new.mapping.slave.write_sdo(new.pdo.index, SubIndex::Index(0), new.num).await?;
+		new.mapping.slave.sdo_write(new.pdo.index, SubIndex::Index(0), new.num).await?;
 		Ok(new)
 	}
 	/// set the given sdo item to be transmitted
@@ -128,7 +128,7 @@ impl<'b> PdoMapping<'b> {
 		assert!(self.num < self.pdo.num, "maximum number of pdo entries reached");
 		log::debug!("  push {:x} {}: {:x}", self.pdo.index, self.num+1, ((sdo.index as u32) << 16) | ((sdo.sub as u32) << 4) | ((sdo.field.bitlen as u32) & 0xff));
 		
-		self.mapping.slave.write_sdo::<u32>(
+		self.mapping.slave.sdo_write::<u32>(
 			self.pdo.index, 
 			SubIndex::Index(self.num+1), 
 			((sdo.index as u32) << 16) | ((sdo.sub as u32) << 8) | ((sdo.field.bitlen as u32) & 0xff),
@@ -141,7 +141,7 @@ impl<'b> PdoMapping<'b> {
 	pub async fn count<T: PduData + DType>(&mut self, sdo: &SubItem<T>) -> Result<Field<T>, Error> {
 		assert!(self.num < self.pdo.num, "maximum number of pdo entries reached");
 		assert_eq!(
-			self.mapping.slave.read_sdo::<u32>(
+			self.mapping.slave.sdo_read::<u32>(
 				self.pdo.index, 
 				SubIndex::Index(self.num+1),
 				).await?, 
@@ -158,7 +158,7 @@ impl<'b> PdoMapping<'b> {
 	/// must be called or the pdo will contain nothing
 	pub async fn finish(&mut self) -> Result<(), Error> {
 		log::debug!("  finish {:x} {}: {:x}", self.pdo.index, 0, self.num);
-		self.mapping.slave.write_sdo(self.pdo.index, SubIndex::Index(0), self.num).await?;
+		self.mapping.slave.sdo_write(self.pdo.index, SubIndex::Index(0), self.num).await?;
 		Ok(())
 	}
 }
