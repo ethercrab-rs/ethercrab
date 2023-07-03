a0ae7b593a71cbc150bb7c8c8159923bf06b21ce
James Waples <james@wapl.es>
2023-07-03 18:22:31 +0100

    Add replay test using Wireshark capture

---
+++ tests/replay-ek1100-el2828-el2889.rs
@@ -0,0 +1,100 @@
+//! Replay comms between EK1100, EL2828, EL2889. Based on `multiple-groups` demo at time of writing.
+
+mod util;
+
+use env_logger::Env;
+use ethercrab::{error::Error, Client, ClientConfig, PduStorage, SlaveGroup, Timeouts};
+use std::time::Duration;
+use tokio::time::MissedTickBehavior;
+
+const MAX_SLAVES: usize = 16;
+const MAX_PDU_DATA: usize = 1100;
+const MAX_FRAMES: usize = 128;
+
+#[derive(Default)]
+struct Groups {
+    /// EL2889 and EK1100. 2 items, 2 bytes of PDI for 16 output bits.
+    ///
+    /// We'll keep the EK1100 in here as it has no PDI but still needs to live somewhere.
+    slow_outputs: SlaveGroup<2, 2>,
+    /// EL2828. 1 item, 1 byte of PDI for 8 output bits.
+    fast_outputs: SlaveGroup<1, 1>,
+}
+
+#[tokio::test]
+async fn replay_ek1100_el2828_el2889() -> Result<(), Error> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
+    static PDU_STORAGE: PduStorage<MAX_FRAMES, MAX_PDU_DATA> = PduStorage::new();
+
+    let (tx, rx, pdu_loop) = PDU_STORAGE.try_split().expect("can only split once");
+
+    let client = Client::new(
+        pdu_loop,
+        Timeouts::default(),
+        ClientConfig {
+            dc_static_sync_iterations: 100,
+            ..Default::default()
+        },
+    );
+
+    util::spawn_tx_rx("tests/replay-ek1100-el2828-el2889.pcapng", tx, rx);
+
+    // Read configurations from slave EEPROMs and configure devices.
+    let groups = client
+        .init::<MAX_SLAVES, _>(Groups::default(), |groups, slave| match slave.name() {
+            "EL2889" | "EK1100" => Ok(&groups.slow_outputs),
+            "EL2828" => Ok(&groups.fast_outputs),
+            _ => Err(Error::UnknownSlave),
+        })
+        .await
+        .expect("Init");
+
+    let Groups {
+        slow_outputs,
+        fast_outputs,
+    } = groups;
+
+    let mut slow_cycle_time = tokio::time::interval(Duration::from_millis(10));
+    slow_cycle_time.set_missed_tick_behavior(MissedTickBehavior::Skip);
+
+    let el2889 = slow_outputs.slave(&client, 1).expect("EL2889 not present!");
+
+    // Set initial output state
+    el2889.io_raw().1[0] = 0x01;
+    el2889.io_raw().1[1] = 0x80;
+
+    // Animate slow pattern for 8 ticks
+    for _ in 0..8 {
+        slow_outputs.tx_rx(&client).await.expect("TX/RX");
+
+        let (_i, o) = el2889.io_raw();
+
+        // Make a nice pattern on EL2889 LEDs
+        o[0] = o[0].rotate_left(1);
+        o[1] = o[1].rotate_right(1);
+
+        slow_cycle_time.tick().await;
+    }
+
+    let mut fast_cycle_time = tokio::time::interval(Duration::from_millis(5));
+    fast_cycle_time.set_missed_tick_behavior(MissedTickBehavior::Skip);
+
+    // Count up to 255 in binary
+    for _ in 0..255 {
+        fast_outputs.tx_rx(&client).await.expect("TX/RX");
+
+        // Increment every output byte for every slave device by one
+        for slave in fast_outputs.iter(&client) {
+            let (_i, o) = slave.io_raw();
+
+            for byte in o.iter_mut() {
+                *byte = byte.wrapping_add(1);
+            }
+        }
+
+        fast_cycle_time.tick().await;
+    }
+
+    Ok(())
+}
---
+++ tests/util.rs
@@ -0,0 +1,142 @@
+//! Replay comms between EK1100, EL2828, EL2889.
+
+use ethercrab::{error::Error, std::tx_rx_task, PduRx, PduTx};
+use pcap_file::pcapng::{Block, PcapNgReader};
+use smoltcp::wire::{EthernetAddress, EthernetFrame};
+use std::{fs::File, future::Future, pin::Pin, task::Poll};
+
+pub fn spawn_tx_rx(capture_file_path: &str, tx: PduTx<'static>, rx: PduRx<'static>) {
+    let interface = std::env::var("INTERFACE");
+
+    // If INTERFACE env var is present, run using real hardware
+    if let Ok(interface) = interface {
+        log::info!("Running using real hardware on interface {}", interface);
+
+        tokio::spawn(tx_rx_task(&interface, tx, rx).expect("spawn TX/RX task"));
+    }
+    // Otherwise, use mocked TX/RX task
+    else {
+        log::info!("Running dummy TX/RX loop");
+
+        tokio::spawn(dummy_tx_rx_task(capture_file_path, tx, rx).expect("Dummy spawn"));
+    };
+}
+
+struct DummyTxRxFut<'a> {
+    tx: PduTx<'a>,
+    rx: PduRx<'a>,
+
+    capture_file: PcapNgReader<File>,
+
+    /// Packet number from Wireshark capture.
+    packet_number: usize,
+}
+
+impl<'a> DummyTxRxFut<'a> {
+    const MASTER_ADDR: EthernetAddress = EthernetAddress([0x10, 0x10, 0x10, 0x10, 0x10, 0x10]);
+    const REPLY_ADDR: EthernetAddress = EthernetAddress([0x12, 0x10, 0x10, 0x10, 0x10, 0x10]);
+
+    fn next_line(&mut self) -> EthernetFrame<Vec<u8>> {
+        while let Some(block) = self.capture_file.next_block() {
+            self.packet_number += 1;
+
+            // Check if there is no error
+            let block = block.expect("Block error");
+
+            let raw = match block {
+                Block::EnhancedPacket(block) => {
+                    let buf = block.data.to_owned();
+
+                    let buf2 = buf.iter().copied().collect::<Vec<_>>();
+
+                    EthernetFrame::new_checked(buf2).expect("Failed to parse block")
+                }
+                Block::InterfaceDescription(_) => continue,
+                other => panic!(
+                    "Frame {} is not correct type: {:?}",
+                    self.packet_number, other
+                ),
+            };
+
+            if raw.src_addr() != Self::MASTER_ADDR && raw.src_addr() != Self::REPLY_ADDR {
+                panic!(
+                    "Frame {} does not have EtherCAT address (has {:?} instead)",
+                    self.packet_number,
+                    raw.src_addr()
+                );
+            }
+
+            return raw;
+        }
+
+        unreachable!();
+    }
+
+    fn next_line_is_send(&mut self) -> EthernetFrame<Vec<u8>> {
+        let next = self.next_line();
+
+        assert_eq!(next.src_addr(), Self::MASTER_ADDR);
+
+        next
+    }
+
+    fn next_line_is_reply(&mut self) -> EthernetFrame<Vec<u8>> {
+        let next = self.next_line();
+
+        assert_eq!(next.src_addr(), Self::REPLY_ADDR);
+
+        next
+    }
+}
+
+impl Future for DummyTxRxFut<'_> {
+    type Output = Result<(), Error>;
+
+    fn poll(mut self: Pin<&mut Self>, ctx: &mut core::task::Context<'_>) -> Poll<Self::Output> {
+        let mut waker = self.tx.lock_waker();
+
+        let mut buf = [0u8; 1536];
+
+        while let Some(frame) = self.tx.next_sendable_frame() {
+            let expected = self.next_line_is_send();
+
+            frame
+                .send_blocking(&mut buf, |got| {
+                    assert_eq!(expected.as_ref(), got, "TX line {}", self.packet_number);
+
+                    Ok(())
+                })
+                .expect("Failed to send");
+
+            // ---
+
+            let expected = self.next_line_is_reply();
+
+            self.rx.receive_frame(expected.as_ref()).expect("Frame RX")
+        }
+
+        waker.replace(ctx.waker().clone());
+
+        Poll::Pending
+    }
+}
+
+/// Spawn a TX and RX task.
+pub fn dummy_tx_rx_task(
+    capture_file_path: &str,
+    pdu_tx: PduTx<'static>,
+    pdu_rx: PduRx<'static>,
+) -> Result<impl Future<Output = Result<(), Error>>, std::io::Error> {
+    let file_in = File::open(capture_file_path).expect("Error opening file");
+    let pcapng_reader = PcapNgReader::new(file_in).expect("Failed to init PCAP reader");
+
+    let task = DummyTxRxFut {
+        tx: pdu_tx,
+        rx: pdu_rx,
+        capture_file: pcapng_reader,
+        // Indexed from 1 in the Wireshark UI
+        packet_number: 1,
+    };
+
+    Ok(task)
+}
