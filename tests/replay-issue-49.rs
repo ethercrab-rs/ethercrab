2b2c791f7ea56b38342a68d3628494ebf26217a3
James Waples <james@wapl.es>
2023-07-03 22:12:01 +0100

    Add test case with manually applied patch

---
+++ tests/replay-issue-49.rs
@@ -0,0 +1,109 @@
+mod util;
+
+use env_logger::Env;
+use ethercrab::{error::Error, Client, ClientConfig, PduStorage, SlaveGroup, Timeouts};
+use std::{sync::Arc, time::Duration};
+use tokio::time::MissedTickBehavior;
+
+/// Maximum number of slaves that can be stored. This must be a power of 2 greater than 1.
+const MAX_SLAVES: usize = 16;
+/// Maximum PDU data payload size - set this to the max PDI size or higher.
+const MAX_PDU_DATA: usize = 1100;
+/// Maximum number of EtherCAT frames that can be in flight at any one time.
+const MAX_FRAMES: usize = 16;
+/// Maximum total PDI length.
+const PDI_LEN: usize = 64;
+
+static PDU_STORAGE: PduStorage<MAX_FRAMES, MAX_PDU_DATA> = PduStorage::new();
+
+#[tokio::test]
+#[cfg_attr(miri, ignore)]
+async fn replay_issue_49() -> Result<(), Error> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
+    let (tx, rx, pdu_loop) = PDU_STORAGE.try_split().expect("can only split once");
+
+    let client = Arc::new(Client::new(
+        pdu_loop,
+        Timeouts {
+            wait_loop_delay: Duration::from_millis(2),
+            mailbox_response: Duration::from_millis(1000),
+            ..Default::default()
+        },
+        ClientConfig {
+            dc_static_sync_iterations: 0,
+        },
+    ));
+
+    util::spawn_tx_rx("tests/replay-issue-49.pcapng", tx, rx);
+
+    let group = client
+        .init_single_group::<MAX_SLAVES, PDI_LEN>(SlaveGroup::new(|slave| {
+            Box::pin(async {
+                // Special case: if an EL3004 module is discovered, it needs some specific config during
+                // init to function properly
+                match slave.name() {
+                    "EL3004" => {
+                        log::info!("Found EL3004. Configuring...");
+
+                        slave.sdo_write(0x1c12, 0, 0u8).await?;
+                        slave.sdo_write(0x1c13, 0, 0u8).await?;
+
+                        slave.sdo_write(0x1c13, 1, 0x1a00u16).await?;
+                        slave.sdo_write(0x1c13, 2, 0x1a02u16).await?;
+                        slave.sdo_write(0x1c13, 3, 0x1a04u16).await?;
+                        slave.sdo_write(0x1c13, 4, 0x1a06u16).await?;
+                        slave.sdo_write(0x1c13, 0, 4u8).await?;
+                    }
+                    "EL3104" => {
+                        log::info!("Found EL3104. Configuring...");
+
+                        slave.sdo_write(0x1c13, 0, 0u8).await?;
+
+                        slave.sdo_write(0x1c13, 1, 0x1a00u16).await?;
+                        slave.sdo_write(0x1c13, 2, 0x1a02u16).await?;
+                        slave.sdo_write(0x1c13, 3, 0x1a04u16).await?;
+                        slave.sdo_write(0x1c13, 4, 0x1a06u16).await?;
+                        slave.sdo_write(0x1c13, 0, 4u8).await?;
+                    }
+                    _ => {}
+                }
+
+                Ok(())
+            })
+        }))
+        .await
+        .expect("Init");
+
+    log::info!("Discovered {} slaves", group.len());
+
+    for slave in group.iter(&client) {
+        let (i, o) = slave.io_raw();
+
+        log::info!(
+            "-> Slave {:#06x} {} has {} input bytes, {} output bytes",
+            slave.configured_address(),
+            slave.name(),
+            i.len(),
+            o.len(),
+        );
+    }
+
+    let mut tick_interval = tokio::time::interval(Duration::from_millis(5));
+    tick_interval.set_missed_tick_behavior(MissedTickBehavior::Skip);
+
+    loop {
+        group.tx_rx(&client).await.expect("TX/RX");
+
+        // Increment every output byte for every slave device by one
+        for slave in group.iter(&client) {
+            let (_i, o) = slave.io_raw();
+
+            for byte in o.iter_mut() {
+                *byte = byte.wrapping_add(1);
+            }
+        }
+
+        tick_interval.tick().await;
+    }
+}
