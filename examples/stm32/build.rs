f570c94b1951d277d7628c4b7e56fe6895c99e6d
James Waples <james@wapl.es>
2023-07-29 15:10:27 +0100

    More Embassy attempts

--- examples/stm32/build.rs
+++ examples/stm32/build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    println!("cargo:rustc-link-arg=--nmagic");
-    println!("cargo:rustc-link-arg=-Tlink.x");
-    println!("cargo:rustc-link-arg=-Tdefmt.x");
+    println!("cargo:rustc-link-arg-bins=--nmagic");
+    println!("cargo:rustc-link-arg-bins=-Tlink.x");
+    println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
 }
--- examples/stm32/src/main.rs
+++ examples/stm32/src/main.rs
@@ -2,7 +2,13 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
+use core::future::poll_fn;
+use core::task::Poll;
+use defmt::*;
 use embassy_executor::Spawner;
+use embassy_net::driver::Driver;
+use embassy_net::driver::RxToken;
+use embassy_net::driver::TxToken;
 use embassy_net::{Stack, StackResources};
 use embassy_stm32::eth::generic_smi::GenericSMI;
 use embassy_stm32::eth::{Ethernet, PacketQueue};
@@ -11,6 +17,7 @@ use embassy_stm32::rng::Rng;
 use embassy_stm32::time::mhz;
 use embassy_stm32::{bind_interrupts, eth, Config};
 use embedded_io::asynch::Write;
+use rand_core::RngCore;
 use static_cell::make_static;
 use {defmt_rtt as _, panic_probe as _};
 
@@ -20,10 +27,8 @@ bind_interrupts!(struct Irqs {
 
 type Device = Ethernet<'static, ETH, GenericSMI>;
 
-const MTU: usize = 1514;
-
 // #[embassy_executor::task]
-// async fn net_task(stack: &'static Stack<Device<'static, MTU>>) -> ! {
+// async fn net_task(stack: &'static Stack<Device>) -> ! {
 //     stack.run().await
 // }
 
@@ -37,9 +42,15 @@ async fn main(spawner: Spawner) {
 
     defmt::info!("Hello World!");
 
+    // // Generate random seed.
+    // let mut rng = Rng::new(p.RNG);
+    // let mut seed = [0; 8];
+    // rng.fill_bytes(&mut seed);
+    // let seed = u64::from_le_bytes(seed);
+
     let mac_addr = [0x00, 0x00, 0xDE, 0xAD, 0xBE, 0xEF];
 
-    let device = Ethernet::new(
+    let device = &mut *make_static!(Ethernet::new(
         make_static!(PacketQueue::<16, 16>::new()),
         p.ETH,
         Irqs,
@@ -55,7 +66,45 @@ async fn main(spawner: Spawner) {
         GenericSMI::new(),
         mac_addr,
         0,
-    );
+    ));
 
-    loop {}
+    // // let config = embassy_net::Config::dhcpv4(Default::default());
+    // let config = embassy_net::Config {
+    //     ipv4: embassy_net::ConfigV4::None,
+    // };
+
+    // // Init network stack
+    // let stack = &*make_static!(Stack::new(
+    //     device,
+    //     config,
+    //     make_static!(StackResources::<2>::new()),
+    //     seed
+    // ));
+
+    // // Launch network task
+    // defmt::unwrap!(spawner.spawn(net_task(&stack)));
+
+    loop {
+        poll_fn(|cx| {
+            defmt::info!("Poll");
+
+            let Some((rx, tx)) = device.receive(cx) else {
+                return Poll::Pending;
+            };
+
+            // rx.consume(|rx| {
+            //     defmt::info!("--> Rx");
+            // });
+
+            // tx.consume(0, |tx| {
+            //     defmt::info!("--> Tx");
+            // });
+
+            // Poll::Ready((rx, tx))
+            Poll::<()>::Pending
+        })
+        .await;
+
+        defmt::info!("Loop");
+    }
 }
