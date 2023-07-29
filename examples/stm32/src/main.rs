fcc80da1ed3c8f12c23ae36070bfe3a54af11f95
James Waples <james@wapl.es>
2023-07-29 12:37:00 +0100

    Initialise ethernet device

--- examples/stm32/src/main.rs
+++ examples/stm32/src/main.rs
@@ -4,13 +4,21 @@
 
 use embassy_executor::Spawner;
 use embassy_net::{Stack, StackResources};
+use embassy_stm32::eth::generic_smi::GenericSMI;
+use embassy_stm32::eth::{Ethernet, PacketQueue};
+use embassy_stm32::peripherals::ETH;
 use embassy_stm32::rng::Rng;
 use embassy_stm32::time::mhz;
-use embassy_stm32::usb_otg::Driver;
-use embassy_stm32::{bind_interrupts, peripherals, usb_otg, Config};
+use embassy_stm32::{bind_interrupts, eth, Config};
 use embedded_io::asynch::Write;
 use static_cell::make_static;
 use {defmt_rtt as _, panic_probe as _};
+
+bind_interrupts!(struct Irqs {
+    ETH => eth::InterruptHandler;
+});
+
+type Device = Ethernet<'static, ETH, GenericSMI>;
 
 const MTU: usize = 1514;
 
@@ -21,7 +29,33 @@ const MTU: usize = 1514;
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
+    let mut config = Config::default();
+    config.rcc.pll48 = true;
+    config.rcc.sys_ck = Some(mhz(48));
+
+    let p = embassy_stm32::init(config);
+
     defmt::info!("Hello World!");
+
+    let mac_addr = [0x00, 0x00, 0xDE, 0xAD, 0xBE, 0xEF];
+
+    let device = Ethernet::new(
+        make_static!(PacketQueue::<16, 16>::new()),
+        p.ETH,
+        Irqs,
+        p.PA1,
+        p.PA2,
+        p.PC1,
+        p.PA7,
+        p.PC4,
+        p.PC5,
+        p.PG13,
+        p.PB13,
+        p.PG11,
+        GenericSMI::new(),
+        mac_addr,
+        0,
+    );
 
     loop {}
 }
