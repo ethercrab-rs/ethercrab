59e4680335b46a2f255ca06ef52e93e69b816932
James Waples <james@wapl.es>
2023-07-30 14:48:41 +0100

    Cleanup and stuff

--- examples/embassy-stm32/src/main.rs
+++ examples/embassy-stm32/src/main.rs
@@ -51,44 +51,30 @@ async fn tx_rx_task(
 ) -> ! {
     defmt::info!("Spawn TX/RX");
 
+    #[inline(always)]
     fn send_ecat(tx: embassy_stm32::eth::TxToken<'_, '_>, frame: SendableFrame<'_>) {
-        // let len = frame.len();
-
         tx.consume(frame.len(), |tx_buf| {
-            let _ = frame.send_blocking(tx_buf, |_ethernet_frame| {
-                // Frame is copied into `tx_buf` inside `send_blocking` so we don't need to do
-                // anything here. The frame is sent once the outer closure in `tx.consume` ends.
+            let _ = frame
+                .send_blocking(tx_buf, |_ethernet_frame| {
+                    // Frame is copied into `tx_buf` inside `send_blocking` so we don't need to do
+                    // anything here. The frame is sent once the outer closure in `tx.consume` ends.
 
-                Ok(())
-            });
+                    Ok(())
+                })
+                .map_err(|e| defmt::error!("Send blocking: {}", e));
         });
     }
 
     poll_fn(|ctx| {
-        // defmt::info!("Poll");
+        defmt::info!("Poll");
 
         pdu_tx.lock_waker().replace(ctx.waker().clone());
 
         if let Some((rx, tx)) = device.receive(ctx) {
-            // defmt::info!("--> Rx and tx available");
+            defmt::info!("TX WITH RX");
 
             rx.consume(|frame| {
-                // let mut f = smoltcp::wire::EthernetFrame::new_unchecked(frame);
-
-                // if f.ethertype() == smoltcp::wire::EthernetProtocol::Unknown(0x88a4) {
-                //     defmt::info!("--> ECAT RESPONSE!");
-
-                //     defmt::info!(
-                //         "type {:?}, dst {:?} src {:?}",
-                //         f.ethertype(),
-                //         f.dst_addr(),
-                //         f.src_addr()
-                //     );
-                // }
-
-                defmt::unwrap!(pdu_rx.receive_frame(frame).map_err(|_| {
-                    defmt::error!("RX");
-                }));
+                defmt::unwrap!(pdu_rx.receive_frame(frame));
             });
 
             if let Some(ethercat_frame) = pdu_tx.next_sendable_frame() {
@@ -96,22 +82,14 @@ async fn tx_rx_task(
 
                 send_ecat(tx, ethercat_frame);
             }
-
-            // Wake again to continue processing any queued packets
-            ctx.waker().wake_by_ref();
         } else if let Some(tx) = device.transmit(ctx) {
-            // defmt::info!("--> Tx available");
+            defmt::info!("TX ONLY");
 
             if let Some(ethercat_frame) = pdu_tx.next_sendable_frame() {
                 defmt::info!("Sennnddddd");
 
                 send_ecat(tx, ethercat_frame);
-
-                // Wake again to continue processing any queued packets
-                ctx.waker().wake_by_ref();
             }
-        } else {
-            // defmt::info!("--> No stuff");
         }
 
         Poll::<!>::Pending
@@ -122,8 +100,8 @@ async fn tx_rx_task(
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let mut config = Config::default();
-    config.rcc.pll48 = true;
-    config.rcc.sys_ck = Some(mhz(48));
+    // config.rcc.pll48 = true;
+    config.rcc.sys_ck = Some(mhz(96));
 
     let p = embassy_stm32::init(config);
 
@@ -132,7 +110,7 @@ async fn main(spawner: Spawner) {
     let mac_addr = [0x00, 0x00, 0xDE, 0xAD, 0xBE, 0xEF];
 
     let device = Ethernet::new(
-        make_static!(PacketQueue::<2, 2>::new()),
+        make_static!(PacketQueue::<4, 4>::new()),
         p.ETH,
         Irqs,
         p.PA1,
