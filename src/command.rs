3a950dd5acf3d849c184d0009a8130155e9867cb
James Waples <james@wapl.es>
2023-08-17 15:51:34 +0100

    Group read and write commands

--- src/command.rs
+++ src/command.rs
@@ -13,44 +13,10 @@ const FRMW: u8 = 0x0E;
 const LWR: u8 = 0x0B;
 const LRW: u8 = 0x0c;
 
-/// PDU command.
-#[derive(Default, PartialEq, Eq, Debug, Copy, Clone)]
+/// Write commands.
+#[derive(PartialEq, Eq, Debug, Copy, Clone)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
-pub enum Command {
-    /// No operation.
-    #[default]
-    Nop,
-
-    /// APRD.
-    Aprd {
-        /// Auto increment counter.
-        address: u16,
-
-        /// Memory location to read from.
-        register: u16,
-    },
-    /// FPRD.
-    Fprd {
-        /// Configured station address.
-        address: u16,
-
-        /// Memory location to read from.
-        register: u16,
-    },
-    /// BRD.
-    Brd {
-        /// Autoincremented by each slave visited.
-        address: u16,
-
-        /// Memory location to read from.
-        register: u16,
-    },
-    /// LRD.
-    Lrd {
-        /// Logical address.
-        address: u32,
-    },
-
+pub enum Writes {
     /// BWR.
     Bwr {
         /// Autoincremented by each slave visited.
@@ -96,34 +62,90 @@ pub enum Command {
     },
 }
 
+/// Read commands that send no data.
+#[derive(PartialEq, Eq, Debug, Copy, Clone)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub enum Reads {
+    /// APRD.
+    Aprd {
+        /// Auto increment counter.
+        address: u16,
+
+        /// Memory location to read from.
+        register: u16,
+    },
+    /// FPRD.
+    Fprd {
+        /// Configured station address.
+        address: u16,
+
+        /// Memory location to read from.
+        register: u16,
+    },
+    /// BRD.
+    Brd {
+        /// Autoincremented by each slave visited.
+        address: u16,
+
+        /// Memory location to read from.
+        register: u16,
+    },
+    /// LRD.
+    Lrd {
+        /// Logical address.
+        address: u32,
+    },
+}
+
+/// PDU command.
+#[derive(Default, PartialEq, Eq, Debug, Copy, Clone)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub enum Command {
+    /// No operation.
+    #[default]
+    Nop,
+
+    /// Read commands.
+    Read(Reads),
+
+    /// Write commands.
+    Write(Writes),
+}
+
 impl core::fmt::Display for Command {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         match self {
             Command::Nop => write!(f, "NOP"),
-            Command::Aprd { address, register } => {
-                write!(f, "APRD(addr {}, reg {})", address, register)
-            }
-            Command::Fprd { address, register } => {
-                write!(f, "FPRD(addr {}, reg {}", address, register)
-            }
-            Command::Brd { address, register } => {
-                write!(f, "BRD(addr {}, reg {}", address, register)
-            }
-            Command::Lrd { address } => write!(f, "LRD(addr {})", address),
-            Command::Bwr { address, register } => {
-                write!(f, "BWR(addr {}, reg {}", address, register)
-            }
-            Command::Apwr { address, register } => {
-                write!(f, "APWR(addr {}, reg {}", address, register)
-            }
-            Command::Fpwr { address, register } => {
-                write!(f, "FPWR(addr {}, reg {}", address, register)
-            }
-            Command::Frmw { address, register } => {
-                write!(f, "FRMW(addr {}, reg {}", address, register)
-            }
-            Command::Lwr { address } => write!(f, "LWR(addr {})", address),
-            Command::Lrw { address } => write!(f, "LRW(addr {})", address),
+
+            Command::Read(read) => match read {
+                Reads::Aprd { address, register } => {
+                    write!(f, "APRD(addr {}, reg {})", address, register)
+                }
+                Reads::Fprd { address, register } => {
+                    write!(f, "FPRD(addr {}, reg {}", address, register)
+                }
+                Reads::Brd { address, register } => {
+                    write!(f, "BRD(addr {}, reg {}", address, register)
+                }
+                Reads::Lrd { address } => write!(f, "LRD(addr {})", address),
+            },
+
+            Command::Write(write) => match write {
+                Writes::Bwr { address, register } => {
+                    write!(f, "BWR(addr {}, reg {}", address, register)
+                }
+                Writes::Apwr { address, register } => {
+                    write!(f, "APWR(addr {}, reg {}", address, register)
+                }
+                Writes::Fpwr { address, register } => {
+                    write!(f, "FPWR(addr {}, reg {}", address, register)
+                }
+                Writes::Frmw { address, register } => {
+                    write!(f, "FRMW(addr {}, reg {}", address, register)
+                }
+                Writes::Lwr { address } => write!(f, "LWR(addr {})", address),
+                Writes::Lrw { address } => write!(f, "LRW(addr {})", address),
+            },
         }
     }
 }
@@ -134,21 +156,21 @@ impl Command {
         match self {
             Self::Nop => NOP,
 
-            // Reads
-            Self::Aprd { .. } => APRD,
-            Self::Fprd { .. } => FPRD,
-            Self::Brd { .. } => BRD,
-            Self::Lrd { .. } => LRD,
+            Self::Read(read) => match read {
+                Reads::Aprd { .. } => APRD,
+                Reads::Fprd { .. } => FPRD,
+                Reads::Brd { .. } => BRD,
+                Reads::Lrd { .. } => LRD,
+            },
 
-            // Writes
-            Self::Bwr { .. } => BWR,
-            Self::Apwr { .. } => APWR,
-            Self::Fpwr { .. } => FPWR,
-            Self::Frmw { .. } => FRMW,
-            Self::Lwr { .. } => LWR,
-
-            // Read/writes
-            Self::Lrw { .. } => LRW,
+            Self::Write(write) => match write {
+                Writes::Bwr { .. } => BWR,
+                Writes::Apwr { .. } => APWR,
+                Writes::Fpwr { .. } => FPWR,
+                Writes::Frmw { .. } => FRMW,
+                Writes::Lwr { .. } => LWR,
+                Writes::Lrw { .. } => LRW,
+            },
         }
     }
 
@@ -161,21 +183,21 @@ impl Command {
         match *self {
             Command::Nop => arr,
 
-            Command::Aprd { address, register }
-            | Command::Apwr { address, register }
-            | Command::Fprd { address, register }
-            | Command::Fpwr { address, register }
-            | Command::Frmw { address, register }
-            | Command::Brd { address, register }
-            | Command::Bwr { address, register } => {
+            Command::Read(Reads::Aprd { address, register })
+            | Command::Read(Reads::Brd { address, register })
+            | Command::Read(Reads::Fprd { address, register })
+            | Command::Write(Writes::Apwr { address, register })
+            | Command::Write(Writes::Fpwr { address, register })
+            | Command::Write(Writes::Frmw { address, register })
+            | Command::Write(Writes::Bwr { address, register }) => {
                 let buf = le_u16(address, buf);
                 let _buf = le_u16(register, buf);
 
                 arr
             }
-            Command::Lrd { address } | Command::Lwr { address } | Command::Lrw { address } => {
-                address.to_le_bytes()
-            }
+            Command::Read(Reads::Lrd { address })
+            | Command::Write(Writes::Lwr { address })
+            | Command::Write(Writes::Lrw { address }) => address.to_le_bytes(),
         }
     }
 
@@ -186,39 +208,32 @@ impl Command {
         match command_code {
             NOP => Ok((i, Command::Nop)),
 
-            APRD => map(pair(le_u16, le_u16), |(address, register)| Command::Aprd {
-                address,
-                register,
+            APRD => map(pair(le_u16, le_u16), |(address, register)| {
+                Command::Read(Reads::Aprd { address, register })
             })(i),
-            FPRD => map(pair(le_u16, le_u16), |(address, register)| Command::Fprd {
-                address,
-                register,
+            FPRD => map(pair(le_u16, le_u16), |(address, register)| {
+                Command::Read(Reads::Fprd { address, register })
             })(i),
-            BRD => map(pair(le_u16, le_u16), |(address, register)| Command::Brd {
-                address,
-                register,
+            BRD => map(pair(le_u16, le_u16), |(address, register)| {
+                Command::Read(Reads::Brd { address, register })
             })(i),
-            LRD => map(le_u32, |address| Command::Lrd { address })(i),
+            LRD => map(le_u32, |address| Command::Read(Reads::Lrd { address }))(i),
 
-            BWR => map(pair(le_u16, le_u16), |(address, register)| Command::Bwr {
-                address,
-                register,
+            BWR => map(pair(le_u16, le_u16), |(address, register)| {
+                Command::Write(Writes::Bwr { address, register })
             })(i),
-            APWR => map(pair(le_u16, le_u16), |(address, register)| Command::Apwr {
-                address,
-                register,
+            APWR => map(pair(le_u16, le_u16), |(address, register)| {
+                Command::Write(Writes::Apwr { address, register })
             })(i),
-            FPWR => map(pair(le_u16, le_u16), |(address, register)| Command::Fpwr {
-                address,
-                register,
+            FPWR => map(pair(le_u16, le_u16), |(address, register)| {
+                Command::Write(Writes::Fpwr { address, register })
             })(i),
-            FRMW => map(pair(le_u16, le_u16), |(address, register)| Command::Frmw {
-                address,
-                register,
+            FRMW => map(pair(le_u16, le_u16), |(address, register)| {
+                Command::Write(Writes::Frmw { address, register })
             })(i),
-            LWR => map(le_u32, |address| Command::Lwr { address })(i),
+            LWR => map(le_u32, |address| Command::Write(Writes::Lwr { address }))(i),
 
-            LRW => map(le_u32, |address| Command::Lrw { address })(i),
+            LRW => map(le_u32, |address| Command::Write(Writes::Lrw { address }))(i),
 
             other => {
                 fmt::error!("Invalid command code {:#02x}", other);
