80c8eea708c7ffbbd38eb06d1045dd5e8818f6e2
James Waples <james@wapl.es>
2023-08-30 10:46:36 +0100

    Allow configuration of retry behaviour for timed out PDUs

--- src/std/linux/mod.rs
+++ src/std/linux/mod.rs
@@ -92,7 +92,9 @@ impl Future for TxRxFut<'_> {
             Poll::Ready(Err(e)) => {
                 fmt::error!("Receive PDU failed: {}", e);
 
-                return Poll::Ready(Err(Error::ReceiveFrame));
+                // PDU failed but don't exit future - it will either be timed out or retried from
+                // elsewhere in the EtherCrab code.
+                ()
             }
             Poll::Pending => (),
         }
