235da8af530930dbb8a3e2ca16ea595d11823648
James Waples <james@wapl.es>
2023-09-02 11:16:35 +0100

    Implement `std::error::Error` for `Error` when in `std` (#104)

--- src/error.rs
+++ src/error.rs
@@ -91,6 +91,9 @@ pub enum Error {
     },
 }
 
+#[cfg(feature = "std")]
+impl std::error::Error for Error {}
+
 impl core::fmt::Display for Error {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         match self {
