a619c9d7cc3c1505e28eeca033f7a7c33e7b3fd5
James Waples <james@wapl.es>
2023-10-12 21:25:07 +0100

    Make `SlaveState` conversions infallible

--- src/slave_state.rs
+++ src/slave_state.rs
@@ -1,29 +1,16 @@
-use crate::{fmt, pdu_data::PduRead};
-use num_enum::TryFromPrimitive;
-use packed_struct::prelude::*;
+use crate::pdu_data::PduRead;
 
 /// AL (application layer) state for a single device.
 ///
 /// Read from register `0x0130` ([`RegisterAddress::AlStatus`](crate::register::RegisterAddress::AlStatus)).
 ///
 /// Defined in ETG1000.6 6.4.1, ETG1000.6 Table 9.
-#[derive(
-    Debug,
-    Default,
-    Copy,
-    Clone,
-    PartialEq,
-    Eq,
-    PrimitiveEnum,
-    num_enum::TryFromPrimitive,
-    num_enum::IntoPrimitive,
-)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, num_enum::FromPrimitive, num_enum::IntoPrimitive)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 #[cfg_attr(feature = "serde", derive(serde::Serialize))]
 #[repr(u8)]
 pub enum SlaveState {
     /// No state recorded/read/known.
-    #[default]
     None = 0x00,
     /// EtherCAT `INIT` state.
     Init = 0x01,
@@ -35,116 +22,37 @@ pub enum SlaveState {
     SafeOp = 0x04,
     /// EtherCAT `OP` state.
     Op = 0x8,
-    /// State is unknown.
-    Unknown = 0xff,
+    /// State is a combination of above variants or is an unknown value.
+    #[num_enum(catch_all)]
+    Other(u8),
+}
+
+impl Default for SlaveState {
+    fn default() -> Self {
+        Self::None
+    }
 }
 
 impl core::fmt::Display for SlaveState {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        let s = match self {
-            SlaveState::None => "None",
-            SlaveState::Init => "Init",
-            SlaveState::PreOp => "Pre-Operational",
-            SlaveState::Bootstrap => "Bootstrap",
-            SlaveState::SafeOp => "Safe-Operational",
-            SlaveState::Op => "Operational",
-            SlaveState::Unknown => "Unknown",
-        };
-
-        f.write_str(s)
+        match self {
+            SlaveState::None => f.write_str("None"),
+            SlaveState::Init => f.write_str("Init"),
+            SlaveState::PreOp => f.write_str("Pre-Operational"),
+            SlaveState::Bootstrap => f.write_str("Bootstrap"),
+            SlaveState::SafeOp => f.write_str("Safe-Operational"),
+            SlaveState::Op => f.write_str("Operational"),
+            SlaveState::Other(value) => write!(f, "Other({:01x})", value),
+        }
     }
 }
 
 impl PduRead for SlaveState {
     const LEN: u16 = u8::LEN;
 
-    type Error = ();
-
-    fn try_from_slice(slice: &[u8]) -> Result<Self, Self::Error> {
-        Self::try_from_primitive(slice[0]).map_err(|e| {
-            fmt::error!("Failed to decide SlaveState from number {:?}", e.number);
-
-            ()
-        })
-    }
-}
-
-bitflags::bitflags! {
-    /// AL (application layer) state for one or more devices.
-    ///
-    /// Read from register `0x0130` ([`RegisterAddress::AlStatus`](crate::register::RegisterAddress::AlStatus)).
-    ///
-    /// Defined in ETG1000.6 6.4.1, ETG1000.6 Table 9.
-    #[derive(
-        Debug,
-        Default,
-        Copy,
-        Clone,
-        PartialEq,
-        Eq,
-    )]
-
-    pub struct SlaveStates: u8 {
-        /// No state recorded/read/known.
-        const NONE = 0x00;
-        /// EtherCAT `INIT` state.
-        const INIT = 0x01;
-        /// EtherCAT `PRE-OP` state.
-        const PRE_OP = 0x02;
-        /// EtherCAT `SAFE-OP` state.
-        const SAFE_OP = 0x04;
-        /// EtherCAT `OP` state.
-        const OP = 0x8;
-    }
-}
-
-#[cfg(feature = "defmt")]
-impl defmt::Format for SlaveStates {
-    fn format(&self, fmt: defmt::Formatter) {
-        defmt::write!(fmt, "SlaveStates({:02x})", self.bits());
-    }
-}
-
-impl PduRead for SlaveStates {
-    const LEN: u16 = u8::LEN;
-
     type Error = core::convert::Infallible;
 
     fn try_from_slice(slice: &[u8]) -> Result<Self, Self::Error> {
-        let res = Self::from_bits_truncate(slice[0]);
-
-        Ok(res)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn known_status() {
-        let expected = SlaveStates::PRE_OP | SlaveStates::SAFE_OP;
-
-        let res = SlaveStates::try_from_slice(&[0x02 | 0x04]);
-
-        assert_eq!(res, Ok(expected));
-    }
-
-    #[test]
-    fn unknown_status() {
-        let expected = SlaveStates::NONE;
-
-        let res = SlaveStates::try_from_slice(&[0xf0]);
-
-        assert_eq!(res, Ok(expected));
-    }
-
-    #[test]
-    fn unknown_status_and_op() {
-        let expected = SlaveStates::OP;
-
-        let res = SlaveStates::try_from_slice(&[0x08 | 0xf0]);
-
-        assert_eq!(res, Ok(expected));
+        Ok(Self::from(slice[0]))
     }
 }
