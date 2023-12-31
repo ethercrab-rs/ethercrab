f4ea2472497d5799f23bbeeed96ed9baf91e3a63
James Waples <james@wapl.es>
2023-12-31 21:18:59 +0000

    Add final binary size to size-over-time script

--- embassy-size.sh
+++ embassy-size.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-echo -e "commit,date,text,bss,dec" > sizes.csv
+echo -e "commit,date,text,bss,dec,bin" > target/sizes.csv
 
-set -xe
+set -e
 
 for commit in $(git rev-list master)
 do
@@ -11,15 +11,23 @@ do
 
         date=$(git show -s --format=%ci $commit)
 
-        git checkout $commit
+        echo "Commit ${commit} at ${date}"
 
-        out=$(cargo size --release | tail -n1)
+        git checkout $commit --quiet
+
+        out=$(cargo size --release --quiet | tail -n1)
         text=$(echo $out | awk '{print $1}')
         bss=$(echo $out | awk '{print $3}')
         dec=$(echo $out | awk '{print $4}')
 
+        cargo objcopy --release --quiet -- -O binary target/size.bin
+        out=$(wc -c target/size.bin)
+        bin=$(echo $out | awk '{print $1}')
+
         popd
 
-        echo -e "$commit,$date,$text,$bss,$dec" >> sizes.csv
+        echo -e "$commit,$date,$text,$bss,$dec,$bin" >> target/sizes.csv
     fi
 done
+
+echo "Done"
