c026b1d19c0ce9ac110715d3abbdb38821c22d54
James Waples <james@wapl.es>
2024-01-07 22:17:07 +0000

    Remove `SlaveClient` and do some cleanups
    No user visible changes.
    
    `SlaveClient` adds unnecessary indirection and overhead for little to no
    gain, so it can now be removed.
    
    Part of the split of #139.

--- src/eeprom/device_reader.rs
+++ src/eeprom/device_reader.rs
@@ -6,9 +6,8 @@ use crate::{
     error::Error,
     fmt,
     register::RegisterAddress,
-    slave::slave_client::SlaveClient,
+    Client, Command,
 };
-use ethercrab_wire::EtherCrabWireWriteSized;
 
 /// The address of the first proper category, positioned after the fixed fields defined in ETG2010
 /// Table 2.
@@ -19,13 +18,37 @@ pub(crate) const SII_FIRST_CATEGORY_START: u16 = 0x0040u16;
 /// EEPROM data provider that communicates with a physical sub device.
 #[derive(Clone)]
 pub struct DeviceEeprom<'slave> {
-    client: &'slave SlaveClient<'slave>,
+    client: &'slave Client<'slave>,
+    configured_address: u16,
 }
 
 impl<'slave> DeviceEeprom<'slave> {
-    /// Create a new device EEPROM instance.
-    pub fn new(client: &'slave SlaveClient<'slave>) -> Self {
-        Self { client }
+    /// Create a new EEPROM reader instance.
+    pub fn new(client: &'slave Client<'slave>, configured_address: u16) -> Self {
+        Self {
+            client,
+            configured_address,
+        }
+    }
+
+    /// Wait for EEPROM read or write operation to finish and clear the busy flag.
+    async fn wait(&self) -> Result<(), Error> {
+        crate::timer_factory::timeout(self.client.timeouts.eeprom, async {
+            loop {
+                let control =
+                    Command::fprd(self.configured_address, RegisterAddress::SiiControl.into())
+                        .wrap(self.client)
+                        .receive::<SiiControl>()
+                        .await?;
+
+                if !control.busy {
+                    break Ok(());
+                }
+
+                self.client.timeouts.loop_tick().await;
+            }
+        })
+        .await
     }
 }
 
@@ -34,40 +57,31 @@ impl<'slave> EepromDataProvider for DeviceEeprom<'slave> {
         &mut self,
         start_word: u16,
     ) -> Result<impl core::ops::Deref<Target = [u8]>, Error> {
-        let status = self
-            .client
-            .read::<SiiControl>(RegisterAddress::SiiControl.into())
+        let status = Command::fprd(self.configured_address, RegisterAddress::SiiControl.into())
+            .wrap(self.client)
+            .receive::<SiiControl>()
             .await?;
 
         // Clear errors
         if status.has_error() {
             fmt::trace!("Resetting EEPROM error flags");
 
-            self.client
-                .write_slice(
-                    RegisterAddress::SiiControl.into(),
-                    &status.error_reset().pack(),
-                )
+            Command::fpwr(self.configured_address, RegisterAddress::SiiControl.into())
+                .wrap(self.client)
+                .send(status.error_reset())
                 .await?;
         }
 
-        // Set up an SII read. This writes the control word and the register word after it
-        // TODO: Consider either removing context strings or using defmt or something to avoid
-        // bloat.
-        self.client
-            .write_slice(
-                RegisterAddress::SiiControl.into(),
-                &SiiRequest::read(start_word).pack(),
-            )
+        Command::fpwr(self.configured_address, RegisterAddress::SiiControl.into())
+            .wrap(self.client)
+            .send_receive(SiiRequest::read(start_word))
             .await?;
 
-        wait(self.client).await?;
+        self.wait().await?;
 
-        self.client
-            .read_slice(
-                RegisterAddress::SiiData.into(),
-                status.read_size.chunk_len(),
-            )
+        Command::fprd(self.configured_address, RegisterAddress::SiiData.into())
+            .wrap(self.client)
+            .receive_slice(status.read_size.chunk_len())
             .await
             .map(|data| {
                 #[cfg(not(feature = "defmt"))]
@@ -78,22 +92,4 @@ impl<'slave> EepromDataProvider for DeviceEeprom<'slave> {
                 data
             })
     }
-}
-
-/// Wait for EEPROM read or write operation to finish and clear the busy flag.
-async fn wait(slave: &SlaveClient<'_>) -> Result<(), Error> {
-    crate::timer_factory::timeout(slave.timeouts().eeprom, async {
-        loop {
-            let control = slave
-                .read::<SiiControl>(RegisterAddress::SiiControl.into())
-                .await?;
-
-            if !control.busy {
-                break Ok(());
-            }
-
-            slave.timeouts().loop_tick().await;
-        }
-    })
-    .await
 }
