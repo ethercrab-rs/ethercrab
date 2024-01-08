e91cb1659fed78fdaa56d0f03779378ce22aa2d2
James Waples <james@wapl.es>
2024-01-08 11:45:50 +0000

    More stats script tweaks

--- repo-stats.sh
+++ repo-stats.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-echo -e "commit,date,loc_total,loc_code,loc_comments,text,bss,dec,bin,obj" > target/sizes.csv
+echo -e "commit,date,loc_total,loc_code,loc_comments,text,bss,dec,bin" > target/sizes.csv
 
-set -e
+trap 'echo -e  "\nCancelled. Restoring repo."; git checkout --quiet --force master; exit' INT
 
 for commit in $(git rev-list master)
 do
+    date=$(git show -s --format=%ci $commit)
+
+    echo "Commit ${commit} at ${date}"
+
+    git checkout $commit --quiet
+
     if [[ -f "examples/embassy-stm32/Cargo.toml" ]]; then
-        pushd examples/embassy-stm32
-
-        date=$(git show -s --format=%ci $commit)
-
-        echo "Commit ${commit} at ${date}"
-
-        git checkout $commit --quiet
+        pushd examples/embassy-stm32 > /dev/null
 
         out=$(cargo size --release --quiet | tail -n1)
         text=$(echo $out | awk '{print $1}')
@@ -24,14 +24,15 @@ do
         out=$(wc -c target/size.bin)
         bin=$(echo $out | awk '{print $1}')
 
-        obj_size=$(cargo objcopy --release --quiet -- -O binary target/size.bin && wc -c target/size.bin | awk '{print $1}')
-
-        popd
+        popd > /dev/null
     fi
 
     code_stats=$(tokei --type Rust | tail -n2 | head -n1 | awk '{print $3","$4","$5}')
 
-    echo -e "$commit,$date,$code_stats,$text,$bss,$dec,$bin,$obj_size" >> target/sizes.csv
+    echo -n "--> "
+    echo -e "$commit,$date,$code_stats,$text,$bss,$dec,$bin" | tee -a target/sizes.csv
 done
 
 echo "Done"
+
+git checkout master --quiet
