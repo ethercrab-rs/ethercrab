56181afec9a2a350807f80fcb3548c192128d2b1
James Waples <james@wapl.es>
2024-02-22 13:33:26 +0000

    SYNC0 pulse on LAN9252

--- examples/dc.rs
+++ examples/dc.rs
@@ -20,6 +20,56 @@ const CYCLIC_OP_ENABLE: u8 = 0b0000_0001;
 const SYNC0_ACTIVATE: u8 = 0b0000_0010;
 const SYNC1_ACTIVATE: u8 = 0b0000_0100;
 
+mod lan9252 {
+    #[repr(u16)]
+    pub enum Lan9252Register {
+        /// 12.14.24 PDI CONTROL REGISTER.
+        ProcessDataInterface = 0x0140,
+        /// 12.14.29 SYNC/LATCH PDI CONFIGURATION REGISTER.
+        SyncLatchConfig = 0x0151,
+    }
+
+    #[derive(Debug, ethercrab_wire::EtherCrabWireRead)]
+    #[repr(u8)]
+    pub enum SyncLatchDrivePolarity {
+        /// 00: Push-Pull Active Low.
+        PushPullActiveLow = 0b00,
+        /// 01: Open Drain (Active Low).
+        OpenDrainActiveLow = 0b01,
+        /// 10: Push-Pull Active High.
+        PushPullActiveHigh = 0b10,
+        /// 11: Open Source (Active High).
+        OpenSourceActiveHigh = 0b11,
+    }
+
+    /// LAN9252 12.14.29 SYNC/LATCH PDI CONFIGURATION REGISTER
+    #[derive(Debug, ethercrab_wire::EtherCrabWireRead)]
+    #[wire(bytes = 1)]
+    pub struct Lan9252Conf {
+        #[wire(bits = 2)]
+        sync0_drive_polarity: SyncLatchDrivePolarity,
+
+        /// `true` = SYNC0 (output), `false` = `LATCH0` (input).
+        #[wire(bits = 1)]
+        sync0_latch0: bool,
+
+        #[wire(bits = 1)]
+        sync0_map: bool,
+
+        #[wire(bits = 2)]
+        sync1_drive_polarity: SyncLatchDrivePolarity,
+
+        /// `true` = SYNC1 (output), `false` = `LATCH1` (input).
+        #[wire(bits = 1)]
+        sync1_latch1: bool,
+
+        #[wire(bits = 1)]
+        sync1_map: bool,
+    }
+}
+
+use lan9252::*;
+
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
@@ -42,84 +92,57 @@ async fn main() -> Result<(), Error> {
     tokio::spawn(tx_rx_task(&interface, tx, rx).expect("spawn TX/RX task"));
 
     // The group will be in PRE-OP at this point
-    let mut group = client
+    let group = client
         .init_single_group::<MAX_SLAVES, PDI_LEN>()
         .await
         .expect("Init");
 
+    // LAN9252 DC canot be configured in PRE-OP
+    let mut group = group
+        .into_safe_op(&client)
+        .await
+        .expect("PRE-OP -> SAFE-OP");
+
     for slave in group.iter(&client) {
-        // Special configuration is required for some slave devices
-        if slave.name() == "EL3004" {
-            log::info!("Found EL3004. Configuring...");
-
-            // Taken from TwinCAT
-            slave.sdo_write(0x1c12, 0, 0u8).await?;
-            slave.sdo_write(0x1c13, 0, 0u8).await?;
-
-            slave.sdo_write(0x1c13, 1, 0x1a00u16).await?;
-            slave.sdo_write(0x1c13, 2, 0x1a02u16).await?;
-            slave.sdo_write(0x1c13, 3, 0x1a04u16).await?;
-            slave.sdo_write(0x1c13, 4, 0x1a06u16).await?;
-            slave.sdo_write(0x1c13, 0, 4u8).await?;
-        } else if slave.name() == "LAN9252-EVB-HBI" {
+        if slave.name() == "LAN9252-EVB-HBI" {
             log::info!("Found LAN9252 in {:?} state", slave.status().await.ok());
+
+            #[derive(Debug, ethercrab_wire::EtherCrabWireRead)]
+            #[wire(bytes = 2)]
+            pub struct SupportedModes {
+                /// Bit 0 = 1: free run is supported.
+                #[wire(bits = 1)]
+                free_run: bool,
+                /// Bit 1 = 1: Synchronous with SM 2 event is supported.
+                #[wire(bits = 1)]
+                sm2: bool,
+                /// Bit 2-3 = 01: DC mode is supported.
+                #[wire(bits = 2)]
+                dc_supported: bool,
+                /// Bit 4-5 = 10: Output shift with SYNC1 event (only DC mode).
+                #[wire(bits = 2)]
+                sync1: bool,
+                /// Bit 14 = 1: dynamic times (measurement through writing of 0x1C32:08).
+                #[wire(pre_skip = 8, bits = 1, post_skip = 1)]
+                dynamic: bool,
+            }
 
             let sync_type = slave.sdo_read::<u16>(0x1c32, 1).await?;
             let cycle_time = slave.sdo_read::<u32>(0x1c32, 2).await?;
-            log::info!("Outputs sync type {sync_type}, cycle time {cycle_time} ns");
+            let min_cycle_time = slave.sdo_read::<u32>(0x1c32, 5).await?;
+            let supported_sync_modes = slave.sdo_read::<SupportedModes>(0x1c32, 4).await?;
+            log::info!("Outputs sync mode {sync_type}, cycle time {cycle_time} ns (min {min_cycle_time} ns), supported modes {supported_sync_modes:?}");
 
             let sync_type = slave.sdo_read::<u16>(0x1c33, 1).await?;
             let cycle_time = slave.sdo_read::<u32>(0x1c33, 2).await?;
-            log::info!("Inputs sync type {sync_type}, cycle time {cycle_time} ns");
-
-            #[repr(u16)]
-            enum Lan9252Register {
-                /// 12.14.29 SYNC/LATCH PDI CONFIGURATION REGISTER.
-                SyncLatchConfig = 0x0151,
-            }
-
-            #[derive(Debug, ethercrab_wire::EtherCrabWireRead)]
-            #[repr(u8)]
-            enum SyncLatchDrivePolarity {
-                /// 00: Push-Pull Active Low.
-                PushPullActiveLow = 0b00,
-                /// 01: Open Drain (Active Low).
-                OpenDrainActiveLow = 0b01,
-                /// 10: Push-Pull Active High.
-                PushPullActiveHigh = 0b10,
-                /// 11: Open Source (Active High).
-                OpenSourceActiveHigh = 0b11,
-            }
-
-            /// LAN9252 12.14.29 SYNC/LATCH PDI CONFIGURATION REGISTER
-            #[derive(Debug, ethercrab_wire::EtherCrabWireRead)]
-            #[wire(bytes = 1)]
-            struct Lan9252Conf {
-                #[wire(bits = 2)]
-                sync0_drive_polarity: SyncLatchDrivePolarity,
-
-                /// `true` = SYNC0 (output), `false` = `LATCH0` (input).
-                #[wire(bits = 1)]
-                sync0_latch0: bool,
-
-                #[wire(bits = 1)]
-                sync0_map: bool,
-
-                #[wire(bits = 2)]
-                sync1_drive_polarity: SyncLatchDrivePolarity,
-
-                /// `true` = SYNC1 (output), `false` = `LATCH1` (input).
-                #[wire(bits = 1)]
-                sync1_latch1: bool,
-
-                #[wire(bits = 1)]
-                sync1_map: bool,
-            }
+            let min_cycle_time = slave.sdo_read::<u32>(0x1c33, 5).await?;
+            let supported_sync_modes = slave.sdo_read::<SupportedModes>(0x1c33, 4).await?;
+            log::info!("Inputs sync mode {sync_type}, cycle time {cycle_time} ns (min {min_cycle_time} ns), supported modes {supported_sync_modes:?}");
 
             let v = slave
                 .register_read::<Lan9252Conf>(Lan9252Register::SyncLatchConfig as u16)
                 .await
-                .expect("0x0151u16");
+                .expect("LAN9252 SyncLatchConfig");
 
             log::info!("LAN9252 config reg 0x0151: {:?}", v);
 
@@ -131,11 +154,8 @@ async fn main() -> Result<(), Error> {
                     .await
                     .expect("DcSyncActive");
 
-                // SOEM does it :shrug:. 0x0980 is just marked "reserved" in ETG1000.4.
-                slave
-                    .register_write(0x980u16, 0u8)
-                    .await
-                    .expect("DcSyncActive");
+                // // SOEM does it :shrug:. 0x0980 is just marked "reserved" in ETG1000.4.
+                // slave.register_write(0x980u16, 0u8).await.expect("0x980u16");
 
                 let device_time = slave
                     .register_read::<i64>(RegisterAddress::DcSystemTime)
@@ -144,29 +164,47 @@ async fn main() -> Result<(), Error> {
 
                 log::info!("Device time {}", device_time);
 
+                let sync0_cycle_time = 100_000;
+                let sync1_cycle_time = 100_000;
+                let cycle_shift = 1;
+
+                let true_cycle_time =
+                    ((sync1_cycle_time / sync0_cycle_time) + 1) * sync0_cycle_time;
+
+                // 100ms
+                let first_pulse_delay = 100000000;
+
+                let t = (device_time + first_pulse_delay) / true_cycle_time * true_cycle_time
+                    + true_cycle_time
+                    + cycle_shift;
+
+                log::info!("t: {}", t);
+
                 slave
-                    .register_write(RegisterAddress::DcSyncStartTime, device_time + 10_000)
+                    .register_write(RegisterAddress::DcSyncStartTime, t)
                     .await
                     .expect("DcSyncStartTime");
 
                 // Cycle time in nanoseconds
                 slave
-                    .register_write(RegisterAddress::DcSync0CycleTime, 10_000)
+                    .register_write(RegisterAddress::DcSync0CycleTime, sync0_cycle_time)
                     .await
                     .expect("DcSync0CycleTime");
+                slave
+                    .register_write(RegisterAddress::DcSync1CycleTime, sync1_cycle_time)
+                    .await
+                    .expect("DcSync1CycleTime");
 
-                // slave
-                //     .register_write(RegisterAddress::DcSyncActive, SYNC0_ACTIVATE)
-                //     .await
-                //     .expect("DcSyncActive");
+                slave
+                    .register_write(
+                        RegisterAddress::DcSyncActive,
+                        SYNC0_ACTIVATE | SYNC1_ACTIVATE | CYCLIC_OP_ENABLE,
+                    )
+                    .await
+                    .expect("DcSyncActive");
             }
         }
     }
-
-    let mut group = group
-        .into_safe_op(&client)
-        .await
-        .expect("PRE-OP -> SAFE-OP");
 
     log::info!("Group has {} slaves", group.len());
 
@@ -190,19 +228,6 @@ async fn main() -> Result<(), Error> {
     group.tx_rx(&client).await.expect("TX/RX");
 
     let mut group = group.into_op(&client).await.expect("SAFE-OP -> OP");
-
-    // Enable cyclic op for all devices
-    for slave in group.iter(&client) {
-        // Enabling CYCLIC_OP_ENABLE gives "Failed to claim receiving frame" if not in OP state,
-        // which is a bizarre error to encounter.
-        slave
-            .register_write(
-                RegisterAddress::DcSyncActive,
-                SYNC0_ACTIVATE | CYCLIC_OP_ENABLE,
-            )
-            .await
-            .expect("DcSyncActive OP");
-    }
 
     loop {
         group.tx_rx(&client).await.expect("TX/RX");
