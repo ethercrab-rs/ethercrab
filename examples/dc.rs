2e60ecc981194f44a1d06c1eac1bdb83be5818f9
James Waples <james@wapl.es>
2024-02-22 20:20:53 +0000

    Make LAN9252s sync, although still with WD timeout

--- examples/dc.rs
+++ examples/dc.rs
@@ -23,6 +23,7 @@ const CYCLIC_OP_ENABLE: u8 = 0b0000_0001;
 const SYNC0_ACTIVATE: u8 = 0b0000_0010;
 const SYNC1_ACTIVATE: u8 = 0b0000_0100;
 
+#[allow(unused)]
 mod lan9252 {
     #[repr(u16)]
     pub enum Lan9252Register {
@@ -73,7 +74,28 @@ mod lan9252 {
 
 use lan9252::*;
 
-const TICK_INTERVAL: Duration = Duration::from_millis(5);
+#[allow(unused)]
+#[derive(Debug, ethercrab_wire::EtherCrabWireRead)]
+#[wire(bytes = 2)]
+pub struct SupportedModes {
+    /// Bit 0 = 1: free run is supported.
+    #[wire(bits = 1)]
+    free_run: bool,
+    /// Bit 1 = 1: Synchronous with SM 2 event is supported.
+    #[wire(bits = 1)]
+    sm2: bool,
+    /// Bit 2-3 = 01: DC mode is supported.
+    #[wire(bits = 2)]
+    dc_supported: bool,
+    /// Bit 4-5 = 10: Output shift with SYNC1 event (only DC mode).
+    #[wire(bits = 2)]
+    sync1: bool,
+    /// Bit 14 = 1: dynamic times (measurement through writing of 0x1C32:08).
+    #[wire(pre_skip = 8, bits = 1, post_skip = 1)]
+    dynamic: bool,
+}
+
+const TICK_INTERVAL: Duration = Duration::from_millis(6);
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -96,7 +118,7 @@ async fn main() -> Result<(), Error> {
             ..Timeouts::default()
         },
         ClientConfig {
-            dc_static_sync_iterations: 1000,
+            dc_static_sync_iterations: 10_000,
             ..ClientConfig::default()
         },
     ));
@@ -109,17 +131,23 @@ async fn main() -> Result<(), Error> {
         .await
         .expect("Init");
 
-    // for slave in group.iter(&client) {
-    //     if slave.name() == "LAN9252-EVB-HBI" {
-    //         log::info!("Found LAN9252 in {:?} state", slave.status().await.ok());
+    for slave in group.iter(&client) {
+        if slave.name() == "LAN9252-EVB-HBI" {
+            log::info!("Found LAN9252 in {:?} state", slave.status().await.ok());
 
-    //         // Sync mode 02 = SYNC0
-    //         slave
-    //             .sdo_write(0x1c32, 1, 2u16)
-    //             .await
-    //             .expect("Set sync mode");
-    //     }
-    // }
+            // Sync mode 02 = SYNC0
+            slave
+                .sdo_write(0x1c32, 1, 2u16)
+                .await
+                .expect("Set sync mode");
+
+            // Adding this seems to make the second LAN9252 converge much more quickly
+            slave
+                .sdo_write(0x1c32, 0x0a, TICK_INTERVAL.as_nanos() as u32)
+                .await
+                .expect("Set cycle time");
+        }
+    }
 
     let client2 = client.clone();
     let group_len = group.len() as u16;
@@ -128,8 +156,9 @@ async fn main() -> Result<(), Error> {
     // This visibly moves the second LAN9252 sync pulse back to 0. Very impressive.
     tokio::spawn(async move {
         let mut sync_tick = tokio::time::interval(Duration::from_millis(1));
-
         sync_tick.set_missed_tick_behavior(MissedTickBehavior::Skip);
+
+        // TODO: Replace with real system time
         let fake_systime = Instant::now();
 
         loop {
@@ -150,26 +179,6 @@ async fn main() -> Result<(), Error> {
         if slave.name() == "LAN9252-EVB-HBI" {
             log::info!("Found LAN9252 in {:?} state", slave.status().await.ok());
 
-            #[derive(Debug, ethercrab_wire::EtherCrabWireRead)]
-            #[wire(bytes = 2)]
-            pub struct SupportedModes {
-                /// Bit 0 = 1: free run is supported.
-                #[wire(bits = 1)]
-                free_run: bool,
-                /// Bit 1 = 1: Synchronous with SM 2 event is supported.
-                #[wire(bits = 1)]
-                sm2: bool,
-                /// Bit 2-3 = 01: DC mode is supported.
-                #[wire(bits = 2)]
-                dc_supported: bool,
-                /// Bit 4-5 = 10: Output shift with SYNC1 event (only DC mode).
-                #[wire(bits = 2)]
-                sync1: bool,
-                /// Bit 14 = 1: dynamic times (measurement through writing of 0x1C32:08).
-                #[wire(pre_skip = 8, bits = 1, post_skip = 1)]
-                dynamic: bool,
-            }
-
             let sync_type = slave.sdo_read::<u16>(0x1c32, 1).await?;
             let cycle_time = slave.sdo_read::<u32>(0x1c32, 2).await?;
             let min_cycle_time = slave.sdo_read::<u32>(0x1c32, 5).await?;
@@ -188,33 +197,36 @@ async fn main() -> Result<(), Error> {
                 .expect("LAN9252 SyncLatchConfig");
 
             log::info!("LAN9252 config reg 0x0151: {:?}", v);
+        }
 
-            // Table 27 – Distributed Clock sync parameter
-            // First slave only. TODO: First DC supporting slave
-            // if slave.configured_address() == 0x1000 {
-            //     log::info!("--> First device configuring for DC");
+        // Table 27 – Distributed Clock sync parameter
+        // First slave only. TODO: First DC supporting slave
+        // if slave.configured_address() == 0x1000 {
+        //     log::info!("--> First device configuring for DC");
+
+        if slave.supports_dc() {
+            log::info!("{} supports DC", slave.name());
 
             // Disable cyclic op
             slave
                 .register_write(RegisterAddress::DcSyncActive, 0u8)
                 .await
-                .expect("DcSyncActive");
+                .expect("DcSyncActive disable");
 
             let device_time = slave
-                .register_read::<i64>(RegisterAddress::DcSystemTime)
+                .register_read::<u64>(RegisterAddress::DcSystemTime)
                 .await
                 .expect("DcSystemTime");
 
             log::info!("Device time {}", device_time);
 
-            let sync0_cycle_time = TICK_INTERVAL.as_nanos() as i64;
-            let sync1_cycle_time = TICK_INTERVAL.as_nanos() as i64;
-            let cycle_shift = 0;
+            let sync0_cycle_time = TICK_INTERVAL.as_nanos() as u64;
+            let sync1_cycle_time = TICK_INTERVAL.as_nanos() as u64;
+            let cycle_shift = (TICK_INTERVAL / 2).as_nanos() as u64;
 
             let true_cycle_time = ((sync1_cycle_time / sync0_cycle_time) + 1) * sync0_cycle_time;
 
-            // 100ms
-            let first_pulse_delay = 100000000;
+            let first_pulse_delay = Duration::from_millis(100).as_nanos() as u64;
 
             let t = (device_time + first_pulse_delay) / true_cycle_time * true_cycle_time
                 + true_cycle_time
@@ -240,20 +252,23 @@ async fn main() -> Result<(), Error> {
             slave
                 .register_write(
                     RegisterAddress::DcSyncActive,
+                    // CYCLIC_OP_ENABLE causes SM watchdog timeouts when going into OP
                     SYNC0_ACTIVATE | CYCLIC_OP_ENABLE,
                 )
                 .await
-                .expect("DcSyncActive");
+                .expect("DcSyncActive enable");
         }
-        // }
     }
+
+    log::info!("Group has {} slaves", group.len());
+
+    let mut tick_interval = tokio::time::interval(TICK_INTERVAL);
+    tick_interval.set_missed_tick_behavior(MissedTickBehavior::Skip);
 
     let mut group = group
         .into_safe_op(&client)
         .await
         .expect("PRE-OP -> SAFE-OP");
-
-    log::info!("Group has {} slaves", group.len());
 
     for slave in group.iter(&client) {
         let (i, o) = slave.io_raw();
@@ -267,8 +282,44 @@ async fn main() -> Result<(), Error> {
         );
     }
 
-    let mut tick_interval = tokio::time::interval(TICK_INTERVAL);
-    tick_interval.set_missed_tick_behavior(MissedTickBehavior::Skip);
+    // let mut now = Instant::now();
+
+    // loop {
+    //     // group.tx_rx(&client).await.expect("TX/RX");
+
+    //     if now.elapsed() >= Duration::from_millis(1000) {
+    //         now = Instant::now();
+
+    //         for slave in group.iter(&client) {
+    //             println!(
+    //                 "sys time {} offs {}, diff {}",
+    //                 slave
+    //                     .register_read::<u64>(RegisterAddress::DcSystemTime)
+    //                     .await
+    //                     .unwrap_or(0),
+    //                 slave
+    //                     .register_read::<u64>(RegisterAddress::DcSystemTimeOffset)
+    //                     .await
+    //                     .unwrap_or(0),
+    //                 slave
+    //                     .register_read::<u32>(RegisterAddress::DcSystemTimeDifference)
+    //                     .await
+    //                     .map(|value| {
+    //                         let value = i32::from_ne_bytes(value.to_ne_bytes());
+
+    //                         if value == i32::MIN {
+    //                             -((value & !i32::MIN) as i32)
+    //                         } else {
+    //                             (value & !i32::MIN) as i32
+    //                         }
+    //                     })
+    //                     .unwrap_or(0)
+    //             );
+    //         }
+    //     }
+
+    //     tick_interval.tick().await;
+    // }
 
     // Provide valid outputs before transition. LAN9252 will timeout going into OP if outputs are
     // not present.
