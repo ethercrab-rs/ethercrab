d95ea97eeaad85a11ab60dfe25dcecc37c114bbb
James Waples <james@wapl.es>
2024-04-19 11:11:25 +0100

    C5-E doesn't seem to support DC

--- ds402/src/main.rs
+++ ds402/src/main.rs
@@ -1,241 +1,306 @@
-//! Configure Distributed Clocks (DC) for EK1100 and a couple of other modules.
+//! Configure a Nanotec C5-E EtherCAT stepper drive.
 //!
-//! Please note this example uses experimental features and should not be used as a reference for
-//! other code. It is here (currently) primarily to help develop EtherCrab.
+//! Motor used for testing is AS5918M2804-E with 500PPR encoder.
 
+use anyhow::Context;
 use env_logger::Env;
 use ethercrab::{
-    error::Error,
-    slave_group::{CycleInfo, DcConfiguration},
+    ds402::{Ds402, Ds402Sm, StatusWord},
     std::{ethercat_now, tx_rx_task},
-    Client, ClientConfig, DcSync, PduStorage, RegisterAddress, Timeouts,
+    Client, ClientConfig, PduStorage, Timeouts,
 };
-use futures_lite::StreamExt;
 use std::{
     sync::{
         atomic::{AtomicBool, Ordering},
         Arc,
     },
-    thread,
-    time::{Duration, Instant},
+    time::Duration,
 };
-use ta::indicators::ExponentialMovingAverage;
-use ta::Next;
+use tokio::time::MissedTickBehavior;
 
-/// Maximum number of slaves that can be stored. This must be a power of 2 greater than 1.
+/// Maximum number of slaves that can be stored.
 const MAX_SLAVES: usize = 16;
+/// Maximum PDU data payload size - set this to the max PDI size or higher.
 const MAX_PDU_DATA: usize = PduStorage::element_size(1100);
-const MAX_FRAMES: usize = 32;
+/// Maximum number of EtherCAT frames that can be in flight at any one time.
+const MAX_FRAMES: usize = 16;
+/// Maximum total PDI length.
 const PDI_LEN: usize = 64;
 
 static PDU_STORAGE: PduStorage<MAX_FRAMES, MAX_PDU_DATA> = PduStorage::new();
 
-const TICK_INTERVAL: Duration = Duration::from_millis(2);
+/// C5-E manual page 127 "Error number"
+#[derive(ethercrab::EtherCrabWireRead, Debug)]
+#[allow(unused)]
+#[wire(bytes = 4)]
+struct C5Error {
+    #[wire(bytes = 2)]
+    pub code: u16,
+    #[wire(bytes = 1)]
+    pub class: u8,
+    #[wire(bytes = 1)]
+    pub number: u8,
+}
 
-fn main() -> Result<(), Error> {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
     let interface = std::env::args()
         .nth(1)
         .expect("Provide network interface as first argument.");
 
+    log::info!("Starting C5-E demo...");
+    log::info!("Run with RUST_LOG=ethercrab=debug or =trace for debug information");
+
     let (tx, rx, pdu_loop) = PDU_STORAGE.try_split().expect("can only split once");
 
     let client = Arc::new(Client::new(
         pdu_loop,
-        Timeouts::default(),
+        Timeouts {
+            wait_loop_delay: Duration::from_millis(2),
+            mailbox_response: Duration::from_millis(1000),
+            ..Default::default()
+        },
         ClientConfig::default(),
     ));
 
-    let mut tick_interval = smol::Timer::interval(TICK_INTERVAL);
+    tokio::spawn(tx_rx_task(&interface, tx, rx).expect("spawn TX/RX task"));
 
-    smol::spawn(tx_rx_task(&interface, tx, rx).expect("spawn TX/RX task")).detach();
+    let mut group = client
+        .init_single_group::<MAX_SLAVES, PDI_LEN>(ethercat_now)
+        .await
+        .expect("Init");
 
-    // Wait for TX/RX loop to start
-    thread::sleep(Duration::from_millis(200));
+    for slave in group.iter(&client) {
+        // Assuming all connected SubDevices are C5-Es here
 
-    #[cfg(target_os = "linux")]
-    thread_priority::set_current_thread_priority(thread_priority::ThreadPriority::Crossplatform(
-        thread_priority::ThreadPriorityValue::try_from(48u8).unwrap(),
-    ))
-    .expect("Main thread prio");
-
-    smol::block_on(async {
-        let mut group = client
-            .init_single_group::<MAX_SLAVES, PDI_LEN>(ethercat_now)
+        // Manual section 4.8 Setting the motor data
+        // 1.8deg step, so 50 pole pairs
+        slave
+            .sdo_write(0x2030, 0, 50u32)
             .await
-            .expect("Init");
-
-        // The group will be in PRE-OP at this point
-
-        for mut slave in group.iter(&client) {
-            // Enable SYNC0
-            slave.set_dc_sync(DcSync::Sync0);
-        }
-
-        log::info!("Group has {} slaves", group.len());
-
-        let mut averages = Vec::new();
-
-        for _ in 0..group.len() {
-            averages.push(ExponentialMovingAverage::new(64).unwrap());
-        }
-
-        log::info!("Moving into PRE-OP with PDI");
-
-        let mut group = group.into_pre_op_pdi(&client).await?;
-
-        log::info!("Done. PDI available. Waiting for SubDevices to align");
-
-        // Repeatedly send group PDI and sync frame to align all SubDevice clocks. We use an
-        // exponential moving average of each SubDevice's deviation from the EtherCAT System Time
-        // (the time in the DC reference SubDevice) and take the maximum deviation. When that is
-        // below 100ns (arbitraily chosen value for this demo), we call the sync good enough and
-        // exit the loop.
-        loop {
-            group.tx_rx_sync_system_time(&client).await.expect("TX/RX");
-
-            let mut max_deviation = 0;
-
-            for (s1, ema) in group.iter(&client).zip(averages.iter_mut()) {
-                let diff = match s1
-                    .register_read::<u32>(RegisterAddress::DcSystemTimeDifference)
-                    .await
-                {
-                    Ok(value) =>
-                    // The returned value is NOT in two's compliment, rather the upper bit specifies
-                    // whether the number in the remaining bits is odd or even, so we convert the
-                    // value to `i32` using that logic here.
-                    {
-                        let flag = 0b1u32 << 31;
-
-                        if value >= flag {
-                            // Strip off negative flag bit and negate value as normal
-                            -((value & !flag) as i32)
-                        } else {
-                            value as i32
-                        }
-                    }
-                    Err(Error::WorkingCounter { .. }) => 0,
-                    Err(e) => return Err(e),
-                };
-
-                let ema_next = ema.next(diff as f64);
-
-                max_deviation = max_deviation.max(ema_next.abs() as u32);
-            }
-
-            log::debug!("--> Max deviation {} ns", max_deviation);
-
-            // Less than 100ns max deviation as an example threshold.
-            // <https://github.com/OpenEtherCATsociety/SOEM/issues/487#issuecomment-786245585>
-            // mentions less than 100us as a good enough value as well.
-            if max_deviation < 100 {
-                break;
-            }
-
-            tick_interval.next().await;
-        }
-
-        log::info!("Alignment done");
-
-        // SubDevice clocks are aligned. We can turn DC on now.
-        let group = group
-            .configure_dc_sync(
-                &client,
-                DcConfiguration {
-                    // Start SYNC0 100ms in the future
-                    start_delay: Duration::from_millis(100),
-                    // SYNC0 period should be the same as the process data loop in most cases
-                    sync0_period: TICK_INTERVAL,
-                    // Send process data half way through cycle
-                    sync0_shift: TICK_INTERVAL / 2,
-                },
-            )
-            .await?;
-
-        let group = group
-            .into_safe_op(&client)
+            .context("pole pairs")?;
+        // Max motor current in mA.
+        slave
+            .sdo_write(0x2031, 0, 1000u32)
             .await
-            .expect("PRE-OP -> SAFE-OP");
+            .context("max current")?;
+        // Rated motor current in mA
+        slave
+            .sdo_write(0x6075, 0, 2820u32)
+            .await
+            .context("rated currnet")?;
+        // Max motor current, % of rated current in milli-percent, i.e. 1000 is 100%
+        slave
+            .sdo_write(0x6073, 0, 1000u16)
+            .await
+            .context("current %")?;
+        // Max motor current max duration in ms
+        slave
+            .sdo_write(0x203b, 02, 100u32)
+            .await
+            .context("max current duration")?;
+        // Motor type: stepper
+        slave
+            .sdo_write(0x3202, 00, 0x08u32)
+            .await
+            .context("set motor type")?;
+        // Test motor has 500ppr incremental encoder, differential
+        slave
+            .sdo_write(0x2059, 00, 0x0u32)
+            .await
+            .context("encoder kind")?;
+        // Set velocity unit to RPM (factory default)
+        slave
+            .sdo_write(0x60a9, 00, 0x00B44700u32)
+            .await
+            .context("velocity unit RPM")?;
 
-        log::info!("SAFE-OP");
+        // CSV described a bit better in section 7.6.2.2 Related Objects of the manual
+        slave.sdo_write(0x1600, 0, 0u8).await?;
+        // Control word, u16
+        // NOTE: The lower word specifies the field length
+        slave.sdo_write(0x1600, 1, 0x6040_0010u32).await?;
+        // Target velocity, i32
+        slave.sdo_write(0x1600, 2, 0x60ff_0020u32).await?;
+        slave.sdo_write(0x1600, 0, 2u8).await?;
 
-        let term = Arc::new(AtomicBool::new(false));
-        signal_hook::flag::register(signal_hook::consts::SIGINT, Arc::clone(&term))
-            .expect("Register hook");
+        slave.sdo_write(0x1a00, 0, 0u8).await?;
+        // Status word, u16
+        slave.sdo_write(0x1a00, 1, 0x6041_0010u32).await?;
+        // Actual position, i32
+        slave.sdo_write(0x1a00, 2, 0x6064_0020u32).await?;
+        // Actual velocity, i32
+        slave.sdo_write(0x1a00, 3, 0x606c_0020u32).await?;
+        slave.sdo_write(0x1a00, 0, 0x03u8).await?;
 
-        // Request OP state without waiting for all SubDevices to reach it. Allows the immediate
-        // start of the process data cycle, which is required when DC sync is used, otherwise
-        // SubDevices never reach OP, most often timing out with a SyncManagerWatchdog error.
-        let mut group = group.request_into_op(&client).await.expect("SAFE-OP -> OP");
+        slave.sdo_write(0x1c12, 0, 0u8).await?;
+        slave.sdo_write(0x1c12, 1, 0x1600u16).await?;
+        slave.sdo_write(0x1c12, 0, 1u8).await?;
 
-        log::info!("OP requested");
+        slave.sdo_write(0x1c13, 0, 0u8).await?;
+        slave.sdo_write(0x1c13, 1, 0x1a00u16).await?;
+        slave.sdo_write(0x1c13, 0, 1u8).await?;
 
-        let op_request = Instant::now();
+        // Opmode - Cyclic Synchronous Position
+        // slave.write_sdo(0x6060, 0, 0x08).await?;
+        // Opmode - Cyclic Synchronous Velocity
+        slave.sdo_write(0x6060, 0, 0x09u8).await?;
+    }
 
-        // Send PDI and check group state until all SubDevices enter OP state. At this point, we can
-        // exit this loop and enter the main process data loop that does not have the state check
-        // overhead present here.
-        while !group.all_op(&client).await? {
-            let now = Instant::now();
+    let mut group = group.into_op(&client).await.expect("PRE-OP -> OP");
 
-            let (
-                _wkc,
-                CycleInfo {
-                    next_cycle_wait, ..
-                },
-            ) = group.tx_rx_dc(&client).await.expect("TX/RX");
+    log::info!("Slaves moved to OP state");
 
-            smol::Timer::at(now + next_cycle_wait).await;
-        }
+    log::info!("Discovered {} slaves", group.len());
+
+    for slave in group.iter(&client) {
+        let (i, o) = slave.io_raw();
 
         log::info!(
-            "All SubDevices entered OP in {} us",
-            op_request.elapsed().as_micros()
+            "-> Slave {:#06x} {} inputs: {} bytes, outputs: {} bytes",
+            slave.configured_address(),
+            slave.name(),
+            i.len(),
+            o.len()
         );
+    }
 
-        // Main application process data cycle
-        loop {
-            let now = Instant::now();
+    // Run twice to prime PDI
+    group.tx_rx(&client).await.expect("TX/RX");
 
-            let (
-                _wkc,
-                CycleInfo {
-                    next_cycle_wait, ..
-                },
-            ) = group.tx_rx_dc(&client).await.expect("TX/RX");
+    // Read cycle time from servo drive
+    let cycle_time = {
+        let slave = group.slave(&client, 0).unwrap();
 
-            for mut slave in group.iter(&client) {
-                let (_i, o) = slave.io_raw_mut();
+        let base = slave.sdo_read::<u8>(0x60c2, 1).await?;
+        let x10 = slave.sdo_read::<i8>(0x60c2, 2).await?;
 
-                for byte in o.iter_mut() {
-                    *byte = byte.wrapping_add(1);
+        let base = f32::from(base);
+        let x10 = 10.0f32.powi(i32::from(x10));
+
+        let cycle_time_ms = (base * x10) * 1000.0;
+
+        Duration::from_millis(unsafe { cycle_time_ms.round().to_int_unchecked() })
+    };
+
+    log::info!("Cycle time: {} ms", cycle_time.as_millis());
+
+    let mut cyclic_interval = tokio::time::interval(cycle_time);
+    cyclic_interval.set_missed_tick_behavior(MissedTickBehavior::Skip);
+
+    let slave = group.slave(&client, 0).expect("No servo!");
+    let mut servo = Ds402Sm::new(Ds402::new(slave).expect("Failed to gather DS402"));
+
+    let mut velocity: i32 = 0;
+
+    let accel = 1;
+    let max_vel = 100;
+
+    let term = Arc::new(AtomicBool::new(false));
+    signal_hook::flag::register(signal_hook::consts::SIGINT, Arc::clone(&term))
+        .expect("Register hook");
+
+    loop {
+        group.tx_rx(&client).await.expect("TX/RX");
+
+        if servo.tick() {
+            let status = servo.status_word();
+            let (i, o) = servo.slave().io_raw_mut();
+
+            let (pos, vel) = {
+                let pos = i32::from_le_bytes(i[2..=5].try_into().unwrap());
+                let vel = i32::from_le_bytes(i[6..=9].try_into().unwrap());
+
+                (pos, vel)
+            };
+
+            println!(
+                "Position: {pos}, velocity: {vel}, status: {status:?} | {:?}",
+                o
+            );
+
+            let vel_cmd = &mut o[2..=5];
+
+            vel_cmd.copy_from_slice(&velocity.to_le_bytes());
+
+            if status.contains(StatusWord::FAULT) {
+                let sl = servo.slave();
+
+                let num_errors = sl
+                    .sdo_read::<u8>(0x1003, 0)
+                    .await
+                    .context("Read error count")?;
+
+                log::error!("Fault! ({})", num_errors);
+
+                for idx in 1..=num_errors {
+                    let code = sl
+                        .sdo_read::<C5Error>(0x1003, idx)
+                        .await
+                        .context("Read error code")?;
+
+                    log::error!("--> {:?}", code);
                 }
+
+                break;
             }
 
-            smol::Timer::at(now + next_cycle_wait).await;
-
-            // Hook exit signal so we can gracefully shutdown
-            if term.load(Ordering::Relaxed) {
-                log::info!("Exiting...");
+            // Normal operation: accelerate up to max speed
+            if !term.load(Ordering::Relaxed) {
+                if vel < max_vel {
+                    velocity += accel;
+                }
+            }
+            // Slow down to a stop when Ctrl + C is pressed
+            else if vel > 0 {
+                velocity -= accel;
+            }
+            // Deceleration is done, we can now exit this loop
+            else {
+                log::info!("Stopping...");
 
                 break;
             }
         }
 
-        let group = group.into_safe_op(&client).await.expect("OP -> SAFE-OP");
+        cyclic_interval.tick().await;
+    }
 
-        log::info!("OP -> SAFE-OP");
+    log::info!("Servo stopped, shutting drive down");
 
-        let group = group.into_pre_op(&client).await.expect("SAFE-OP -> PRE-OP");
+    loop {
+        group.tx_rx(&client).await.expect("TX/RX");
 
-        log::info!("SAFE-OP -> PRE-OP");
+        if servo.tick_shutdown() {
+            break;
+        }
 
-        let _group = group.into_init(&client).await.expect("PRE-OP -> INIT");
+        let status = servo.status_word();
+        let (i, o) = servo.slave().io_raw_mut();
 
-        log::info!("PRE-OP -> INIT, shutdown complete");
+        // In fault state, so don't bother trying to shut down gracefully.
+        if status.contains(StatusWord::FAULT) {
+            break;
+        }
 
-        Ok(())
-    })
+        let (pos, vel) = {
+            let pos = i32::from_le_bytes(i[2..=5].try_into().unwrap());
+            let vel = i32::from_le_bytes(i[6..=9].try_into().unwrap());
+
+            (pos, vel)
+        };
+
+        println!(
+            "Position: {pos}, velocity: {vel}, status: {status:?} | {:?}",
+            o
+        );
+
+        cyclic_interval.tick().await;
+    }
+
+    log::info!("Drive is shut down");
+
+    Ok(())
 }
