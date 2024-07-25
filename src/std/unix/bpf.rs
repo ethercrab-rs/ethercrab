0f1735ad9b3c3e7f7cf9805611b8cdbfde664e9d
James Waples <james@wapl.es>
2024-07-25 15:23:00 +0100

    FreeBSD/NetBSD support (#162)

--- src/std/unix/bpf.rs
+++ src/std/unix/bpf.rs
@@ -14,25 +14,65 @@ use std::{
     os::unix::io::{AsFd, AsRawFd, BorrowedFd, RawFd},
 };
 
+#[cfg(not(target_os = "netbsd"))]
+use libc::bpf_hdr;
+
+#[cfg(target_os = "netbsd")]
+#[allow(dead_code, non_camel_case_types)]
+// Unsure why  `libc` doesn't expose `bpf_hdr` when it's defined in NetBSD's man page
+// <https://man.netbsd.org/bpf.4#BPF%20HEADER> but ah well, here it is defined inline.
+struct bpf_hdr {
+    pub bh_tstamp: libc::timeval,
+    pub bh_caplen: u32,
+    pub bh_datalen: u32,
+    pub bh_hdrlen: libc::c_ushort,
+}
+
 /// set interface
-#[cfg(any(target_os = "macos", target_os = "openbsd"))]
+#[cfg(any(
+    target_os = "macos",
+    target_os = "ios",
+    target_os = "netbsd",
+    target_os = "openbsd",
+    target_os = "freebsd"
+))]
 const BIOCSETIF: libc::c_ulong = 0x8020426c;
 /// get buffer length
-#[cfg(any(target_os = "macos", target_os = "openbsd"))]
+#[cfg(any(
+    target_os = "macos",
+    target_os = "ios",
+    target_os = "netbsd",
+    target_os = "openbsd",
+    target_os = "freebsd"
+))]
 const BIOCGBLEN: libc::c_ulong = 0x40044266;
 /// set immediate/nonblocking read
-#[cfg(any(target_os = "macos", target_os = "openbsd"))]
+#[cfg(any(
+    target_os = "macos",
+    target_os = "ios",
+    target_os = "netbsd",
+    target_os = "openbsd",
+    target_os = "freebsd"
+))]
 const BIOCIMMEDIATE: libc::c_ulong = 0x80044270;
 /// set bpf_hdr struct size
-#[cfg(target_os = "macos")]
+#[cfg(any(target_os = "macos", target_os = "ios", target_os = "netbsd"))]
 const SIZEOF_BPF_HDR: usize = 18;
 /// set bpf_hdr struct size
-#[cfg(target_os = "openbsd")]
+#[cfg(any(target_os = "openbsd", target_os = "freebsd"))]
 const SIZEOF_BPF_HDR: usize = 24;
 /// The actual header length may be larger than the bpf_hdr struct due to aligning
 /// see https://github.com/openbsd/src/blob/37ecb4d066e5566411cc16b362d3960c93b1d0be/sys/net/bpf.c#L1649
 /// and https://github.com/apple/darwin-xnu/blob/8f02f2a044b9bb1ad951987ef5bab20ec9486310/bsd/net/bpf.c#L3580
-#[cfg(any(target_os = "macos", target_os = "openbsd"))]
+/// and https://github.com/NetBSD/src/blob/13d937d9ba3db87c9a898a40a8ed9d2aab2b1b95/sys/net/bpf.c#L1988
+/// and https://github.com/freebsd/freebsd-src/blob/b8afdda360e5915be3c2cf0d1438f511779b03db/sys/net/bpf.c#L133-L134
+#[cfg(any(
+    target_os = "macos",
+    target_os = "ios",
+    target_os = "netbsd",
+    target_os = "openbsd",
+    target_os = "freebsd"
+))]
 const BPF_HDRLEN: usize = (((SIZEOF_BPF_HDR + ETHERNET_HEADER_LEN) + mem::align_of::<u32>() - 1)
     & !(mem::align_of::<u32>() - 1))
     - ETHERNET_HEADER_LEN;
@@ -210,7 +250,7 @@ impl io::Read for BpfDevice {
             let bpf_header = &buffer[0..BPF_HDRLEN];
 
             let bpf_header = unsafe {
-                core::ptr::NonNull::new(bpf_header.as_ptr() as *mut libc::bpf_hdr)
+                core::ptr::NonNull::new(bpf_header.as_ptr() as *mut bpf_hdr)
                     .ok_or(io::Error::other("no BPF header"))?
                     .as_ref()
             };
@@ -227,13 +267,12 @@ impl io::Read for BpfDevice {
         let remaining = len as u32 - BPF_HDRLEN as u32 - frame_len as u32;
 
         // Returned data is aligned to bpf_hdr boundaries
-        let remaining = remaining.next_multiple_of(core::mem::align_of::<libc::bpf_hdr>() as u32);
+        let remaining = remaining.next_multiple_of(core::mem::align_of::<bpf_hdr>() as u32);
 
         // There is at least one more frame in the returned data. Store this in our buffer to return
         // next time `read` is called.
         if remaining > 0 {
-            let start =
-                (BPF_HDRLEN + frame_len).next_multiple_of(core::mem::align_of::<libc::bpf_hdr>());
+            let start = (BPF_HDRLEN + frame_len).next_multiple_of(core::mem::align_of::<bpf_hdr>());
 
             // Store next chunk(s - there could be more than one packet waiting) of [BPF header,
             // Ethernet II frame] in cache for next time round.
