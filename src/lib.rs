53fac1a7709ee177e255d24c17dafd3a8490fdd0
James Waples <james@wapl.es>
2025-02-16 15:07:15 +0000

    Add DS402 raw control and status words

--- src/lib.rs
+++ src/lib.rs
@@ -163,6 +163,7 @@ mod coe;
 mod command;
 mod dc;
 mod dl_status;
+mod ds402;
 mod eeprom;
 pub mod error;
 mod ethernet;
@@ -184,9 +185,11 @@ mod vendors;
 #[cfg(feature = "std")]
 pub mod std;
 
+// TODO: Constrain this
 pub use al_status_code::AlStatusCode;
 pub use coe::SubIndex;
 pub use command::{Command, Reads, WrappedRead, WrappedWrite, Writes};
+pub use ds402::*;
 pub use ethercrab_wire::{
     EtherCrabWireRead, EtherCrabWireReadSized, EtherCrabWireReadWrite, EtherCrabWireSized,
     EtherCrabWireWrite, EtherCrabWireWriteSized,
