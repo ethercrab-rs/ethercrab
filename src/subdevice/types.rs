3a880ebebde4b1017d1217312af08316c5e4c0ce
Theo Lemay <16546293+theol0403@users.noreply.github.com>
2025-11-16 20:42:09 -0800

    Use sync manager types from EEPROM instead of CoE (#330)

--- src/subdevice/types.rs
+++ src/subdevice/types.rs
@@ -1,7 +1,4 @@
-use crate::{
-    eeprom::types::{MailboxProtocols, SyncManagerType},
-    pdi::PdiSegment,
-};
+use crate::{eeprom::types::MailboxProtocols, pdi::PdiSegment};
 use core::fmt::{self, Debug};
 
 /// SubDevice identity information (vendor ID, product ID, etc).
@@ -58,7 +55,6 @@ pub struct MailboxConfig {
     pub(in crate::subdevice) read: Option<Mailbox>,
     pub(in crate::subdevice) write: Option<Mailbox>,
     pub(in crate::subdevice) supported_protocols: MailboxProtocols,
-    pub(in crate::subdevice) coe_sync_manager_types: heapless::Vec<SyncManagerType, 16>,
     pub(in crate::subdevice) has_coe: bool,
     /// True if Complete Access is supported.
     pub(in crate::subdevice) complete_access: bool,
