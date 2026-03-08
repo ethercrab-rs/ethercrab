557f16f654e2f80c29a598181c9fcfc2861b9fed
James Waples <james@wapl.es>
2026-03-07 16:35:59 -0800

    Move examples to `smol` from `tokio`
    There's nothing wrong with using `tokio`, however in testing it has
    poorer jitter and timer resolution than `smol`, so I'm moving the
    examples to use the latter. EtherCrab generally recommands `smol`
    over `tokio` for most use cases.

--- examples/ek1100.rs
+++ examples/ek1100.rs
@@ -18,6 +18,7 @@ use env_logger::Env;
 use ethercrab::{
     MainDevice, MainDeviceConfig, PduStorage, Timeouts, error::Error, std::ethercat_now,
 };
+use futures_lite::StreamExt;
 use std::{
     sync::{
         Arc,
@@ -25,7 +26,6 @@ use std::{
     },
     time::Duration,
 };
-use tokio::time::MissedTickBehavior;
 
 /// Maximum number of SubDevices that can be stored. This must be a power of 2 greater than 1.
 const MAX_SUBDEVICES: usize = 16;
@@ -38,132 +38,133 @@ const PDI_LEN: usize = 64;
 
 static PDU_STORAGE: PduStorage<MAX_FRAMES, MAX_PDU_DATA> = PduStorage::new();
 
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+fn main() -> Result<(), Error> {
+    smol::block_on(async {
+        env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
-    let interface = std::env::args()
-        .nth(1)
-        .expect("Provide network interface as first argument.");
+        let interface = std::env::args()
+            .nth(1)
+            .expect("Provide network interface as first argument.");
 
-    log::info!("Starting EK1100/EK1501 demo...");
-    log::info!(
-        "Ensure an EK1100 or EK1501 is the first SubDevice, with any number of modules connected after"
-    );
-    log::info!("Run with RUST_LOG=ethercrab=debug or =trace for debug information");
-
-    let (tx, rx, pdu_loop) = PDU_STORAGE.try_split().expect("can only split once");
-
-    let maindevice = Arc::new(MainDevice::new(
-        pdu_loop,
-        Timeouts {
-            wait_loop_delay: Duration::from_millis(2),
-            mailbox_response: Duration::from_millis(1000),
-            ..Default::default()
-        },
-        MainDeviceConfig::default(),
-    ));
-
-    #[cfg(target_os = "windows")]
-    std::thread::spawn(move || {
-        ethercrab::std::tx_rx_task_blocking(
-            &interface,
-            tx,
-            rx,
-            ethercrab::std::TxRxTaskConfig { spinloop: false },
-        )
-        .expect("TX/RX task")
-    });
-    #[cfg(not(target_os = "windows"))]
-    tokio::spawn(ethercrab::std::tx_rx_task(&interface, tx, rx).expect("spawn TX/RX task"));
-
-    let group = maindevice
-        .init_single_group::<MAX_SUBDEVICES, PDI_LEN>(ethercat_now)
-        .await
-        .expect("Init");
-
-    log::info!("Discovered {} SubDevices", group.len());
-
-    for subdevice in group.iter(&maindevice) {
-        if subdevice.name() == "EL3004" {
-            log::info!("Found EL3004. Configuring...");
-
-            subdevice.sdo_write(0x1c12, 0, 0u8).await?;
-
-            subdevice
-                .sdo_write_array(0x1c13, &[0x1a00u16, 0x1a02, 0x1a04, 0x1a06])
-                .await?;
-
-            // The `sdo_write_array` call above is equivalent to the following
-            // subdevice.sdo_write(0x1c13, 0, 0u8).await?;
-            // subdevice.sdo_write(0x1c13, 1, 0x1a00u16).await?;
-            // subdevice.sdo_write(0x1c13, 2, 0x1a02u16).await?;
-            // subdevice.sdo_write(0x1c13, 3, 0x1a04u16).await?;
-            // subdevice.sdo_write(0x1c13, 4, 0x1a06u16).await?;
-            // subdevice.sdo_write(0x1c13, 0, 4u8).await?;
-        }
-    }
-
-    let group = group.into_op(&maindevice).await.expect("PRE-OP -> OP");
-
-    for subdevice in group.iter(&maindevice) {
-        let io = subdevice.io_raw();
-
+        log::info!("Starting EK1100/EK1501 demo...");
         log::info!(
-            "-> SubDevice {:#06x} {} inputs: {} bytes, outputs: {} bytes",
-            subdevice.configured_address(),
-            subdevice.name(),
-            io.inputs().len(),
-            io.outputs().len()
+            "Ensure an EK1100 or EK1501 is the first SubDevice, with any number of modules connected after"
         );
-    }
+        log::info!("Run with RUST_LOG=ethercrab=debug or =trace for debug information");
 
-    let mut tick_interval = tokio::time::interval(Duration::from_millis(5));
-    tick_interval.set_missed_tick_behavior(MissedTickBehavior::Skip);
+        let (tx, rx, pdu_loop) = PDU_STORAGE.try_split().expect("can only split once");
 
-    let shutdown = Arc::new(AtomicBool::new(false));
-    signal_hook::flag::register(signal_hook::consts::SIGINT, Arc::clone(&shutdown))
-        .expect("Register hook");
+        let maindevice = Arc::new(MainDevice::new(
+            pdu_loop,
+            Timeouts {
+                wait_loop_delay: Duration::from_millis(2),
+                mailbox_response: Duration::from_millis(1000),
+                ..Default::default()
+            },
+            MainDeviceConfig::default(),
+        ));
 
-    loop {
-        // Graceful shutdown on Ctrl + C
-        if shutdown.load(Ordering::Relaxed) {
-            log::info!("Shutting down...");
+        #[cfg(target_os = "windows")]
+        std::thread::spawn(move || {
+            ethercrab::std::tx_rx_task_blocking(
+                &interface,
+                tx,
+                rx,
+                ethercrab::std::TxRxTaskConfig { spinloop: false },
+            )
+            .expect("TX/RX task")
+        });
+        #[cfg(not(target_os = "windows"))]
+        smol::spawn(ethercrab::std::tx_rx_task(&interface, tx, rx).expect("spawn TX/RX task"))
+            .detach();
 
-            break;
-        }
+        let group = maindevice
+            .init_single_group::<MAX_SUBDEVICES, PDI_LEN>(ethercat_now)
+            .await
+            .expect("Init");
 
-        group.tx_rx(&maindevice).await.expect("TX/RX");
+        log::info!("Discovered {} SubDevices", group.len());
 
-        // Increment every output byte for every SubDevice by one
         for subdevice in group.iter(&maindevice) {
-            let mut o = subdevice.outputs_raw_mut();
+            if subdevice.name() == "EL3004" {
+                log::info!("Found EL3004. Configuring...");
 
-            for byte in o.iter_mut() {
-                *byte = byte.wrapping_add(1);
+                subdevice.sdo_write(0x1c12, 0, 0u8).await?;
+
+                subdevice
+                    .sdo_write_array(0x1c13, &[0x1a00u16, 0x1a02, 0x1a04, 0x1a06])
+                    .await?;
+
+                // The `sdo_write_array` call above is equivalent to the following
+                // subdevice.sdo_write(0x1c13, 0, 0u8).await?;
+                // subdevice.sdo_write(0x1c13, 1, 0x1a00u16).await?;
+                // subdevice.sdo_write(0x1c13, 2, 0x1a02u16).await?;
+                // subdevice.sdo_write(0x1c13, 3, 0x1a04u16).await?;
+                // subdevice.sdo_write(0x1c13, 4, 0x1a06u16).await?;
+                // subdevice.sdo_write(0x1c13, 0, 4u8).await?;
             }
         }
 
-        tick_interval.tick().await;
-    }
+        let group = group.into_op(&maindevice).await.expect("PRE-OP -> OP");
 
-    let group = group
-        .into_safe_op(&maindevice)
-        .await
-        .expect("OP -> SAFE-OP");
+        for subdevice in group.iter(&maindevice) {
+            let io = subdevice.io_raw();
 
-    log::info!("OP -> SAFE-OP");
+            log::info!(
+                "-> SubDevice {:#06x} {} inputs: {} bytes, outputs: {} bytes",
+                subdevice.configured_address(),
+                subdevice.name(),
+                io.inputs().len(),
+                io.outputs().len()
+            );
+        }
 
-    let group = group
-        .into_pre_op(&maindevice)
-        .await
-        .expect("SAFE-OP -> PRE-OP");
+        let mut tick_interval = smol::Timer::interval(Duration::from_millis(5));
 
-    log::info!("SAFE-OP -> PRE-OP");
+        let shutdown = Arc::new(AtomicBool::new(false));
+        signal_hook::flag::register(signal_hook::consts::SIGINT, Arc::clone(&shutdown))
+            .expect("Register hook");
 
-    let _group = group.into_init(&maindevice).await.expect("PRE-OP -> INIT");
+        loop {
+            // Graceful shutdown on Ctrl + C
+            if shutdown.load(Ordering::Relaxed) {
+                log::info!("Shutting down...");
 
-    log::info!("PRE-OP -> INIT, shutdown complete");
+                break;
+            }
 
-    Ok(())
+            group.tx_rx(&maindevice).await.expect("TX/RX");
+
+            // Increment every output byte for every SubDevice by one
+            for subdevice in group.iter(&maindevice) {
+                let mut o = subdevice.outputs_raw_mut();
+
+                for byte in o.iter_mut() {
+                    *byte = byte.wrapping_add(1);
+                }
+            }
+
+            tick_interval.next().await;
+        }
+
+        let group = group
+            .into_safe_op(&maindevice)
+            .await
+            .expect("OP -> SAFE-OP");
+
+        log::info!("OP -> SAFE-OP");
+
+        let group = group
+            .into_pre_op(&maindevice)
+            .await
+            .expect("SAFE-OP -> PRE-OP");
+
+        log::info!("SAFE-OP -> PRE-OP");
+
+        let _group = group.into_init(&maindevice).await.expect("PRE-OP -> INIT");
+
+        log::info!("PRE-OP -> INIT, shutdown complete");
+
+        Ok(())
+    })
 }
--- examples/multiple-groups.rs
+++ examples/multiple-groups.rs
@@ -11,11 +11,11 @@ use ethercrab::{
     MainDevice, MainDeviceConfig, PduStorage, SubDeviceGroup, Timeouts, error::Error,
     std::ethercat_now,
 };
+use futures_lite::StreamExt;
 use std::{
     sync::Arc,
     time::{Duration, Instant},
 };
-use tokio::time::MissedTickBehavior;
 
 /// Maximum number of SubDevices that can be stored. This must be a power of 2 greater than 1.
 const MAX_SUBDEVICES: usize = 16;
@@ -38,149 +38,146 @@ struct Groups {
     fast_outputs: SubDeviceGroup<1, 1>,
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+fn main() -> Result<(), Error> {
+    smol::block_on(async {
+        env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
-    let interface = std::env::args()
-        .nth(1)
-        .expect("Provide network interface as first argument.");
+        let interface = std::env::args()
+            .nth(1)
+            .expect("Provide network interface as first argument.");
 
-    log::info!("Starting multiple groups demo...");
-    log::info!(
-        "Ensure an EK1100 or EK1501 is the first SubDevice, with an EL2828 and EL2889 following it"
-    );
-    log::info!("Run with RUST_LOG=ethercrab=debug or =trace for debug information");
+        log::info!("Starting multiple groups demo...");
+        log::info!(
+            "Ensure an EK1100 or EK1501 is the first SubDevice, with an EL2828 and EL2889 following it"
+        );
+        log::info!("Run with RUST_LOG=ethercrab=debug or =trace for debug information");
 
-    let (tx, rx, pdu_loop) = PDU_STORAGE.try_split().expect("can only split once");
+        let (tx, rx, pdu_loop) = PDU_STORAGE.try_split().expect("can only split once");
 
-    let maindevice = MainDevice::new(
-        pdu_loop,
-        Timeouts {
-            wait_loop_delay: Duration::from_millis(2),
-            mailbox_response: Duration::from_millis(1000),
-            ..Default::default()
-        },
-        MainDeviceConfig::default(),
-    );
-
-    #[cfg(target_os = "windows")]
-    std::thread::spawn(move || {
-        ethercrab::std::tx_rx_task_blocking(
-            &interface,
-            tx,
-            rx,
-            ethercrab::std::TxRxTaskConfig { spinloop: false },
-        )
-        .expect("TX/RX task")
-    });
-    // Network TX/RX should run in a separate thread to avoid timeouts. Tokio doesn't guarantee a
-    // separate thread is used but this is good enough for an example. If using `tokio`, make sure
-    // the `rt-multi-thread` feature is enabled.
-    #[cfg(not(target_os = "windows"))]
-    tokio::spawn(ethercrab::std::tx_rx_task(&interface, tx, rx).expect("spawn TX/RX task"));
-
-    let maindevice = Arc::new(maindevice);
-
-    // Read configurations from SubDevice EEPROMs and configure devices.
-    let groups = maindevice
-        .init::<MAX_SUBDEVICES, _>(
-            ethercat_now,
-            Groups::default(),
-            |groups: &Groups, subdevice| match subdevice.name() {
-                "EL2889" | "EK1100" | "EK1501" => Ok(&groups.slow_outputs),
-                "EL2828" => Ok(&groups.fast_outputs),
-                _ => Err(Error::UnknownSubDevice),
+        let maindevice = MainDevice::new(
+            pdu_loop,
+            Timeouts {
+                wait_loop_delay: Duration::from_millis(2),
+                mailbox_response: Duration::from_millis(1000),
+                ..Default::default()
             },
-        )
-        .await
-        .expect("Init");
+            MainDeviceConfig::default(),
+        );
 
-    let Groups {
-        slow_outputs,
-        fast_outputs,
-    } = groups;
+        #[cfg(target_os = "windows")]
+        std::thread::spawn(move || {
+            ethercrab::std::tx_rx_task_blocking(
+                &interface,
+                tx,
+                rx,
+                ethercrab::std::TxRxTaskConfig { spinloop: false },
+            )
+            .expect("TX/RX task")
+        });
+        // Network TX/RX should run in a separate thread to avoid timeouts. Tokio doesn't guarantee a
+        // separate thread is used but this is good enough for an example. If using `tokio`, make sure
+        // the `rt-multi-thread` feature is enabled.
+        #[cfg(not(target_os = "windows"))]
+        smol::spawn(ethercrab::std::tx_rx_task(&interface, tx, rx).expect("spawn TX/RX task"))
+            .detach();
 
-    let maindevice_slow = maindevice.clone();
+        let maindevice = Arc::new(maindevice);
 
-    let slow_task = tokio::spawn(async move {
-        let slow_outputs = slow_outputs
-            .into_op(&maindevice_slow)
+        // Read configurations from SubDevice EEPROMs and configure devices.
+        let groups = maindevice
+            .init::<MAX_SUBDEVICES, _>(
+                ethercat_now,
+                Groups::default(),
+                |groups: &Groups, subdevice| match subdevice.name() {
+                    "EL2889" | "EK1100" | "EK1501" => Ok(&groups.slow_outputs),
+                    "EL2828" => Ok(&groups.fast_outputs),
+                    _ => Err(Error::UnknownSubDevice),
+                },
+            )
             .await
-            .expect("PRE-OP -> OP");
+            .expect("Init");
 
-        let mut slow_cycle_time = tokio::time::interval(Duration::from_millis(3));
-        slow_cycle_time.set_missed_tick_behavior(MissedTickBehavior::Skip);
+        let Groups {
+            slow_outputs,
+            fast_outputs,
+        } = groups;
 
-        let slow_duration = Duration::from_millis(250);
+        let maindevice_slow = maindevice.clone();
 
-        // Only update "slow" outputs every 250ms using this instant
-        let mut tick = Instant::now();
+        let slow_task = async {
+            let slow_outputs = slow_outputs
+                .into_op(&maindevice_slow)
+                .await
+                .expect("PRE-OP -> OP");
 
-        // EK1100 is first SubDevice, EL2889 is second
-        let el2889 = slow_outputs
-            .subdevice(&maindevice_slow, 1)
-            .expect("EL2889 not present!");
+            let mut slow_cycle_time = smol::Timer::interval(Duration::from_millis(3));
 
-        // Set initial output state
-        el2889.outputs_raw_mut()[0] = 0x01;
-        el2889.outputs_raw_mut()[1] = 0x80;
+            let slow_duration = Duration::from_millis(250);
 
-        loop {
-            let Ok(_) = slow_outputs.tx_rx(&maindevice_slow).await else {
-                break;
-            };
+            // Only update "slow" outputs every 250ms using this instant
+            let mut tick = Instant::now();
 
-            // Increment every output byte for every SubDevice by one
-            if tick.elapsed() > slow_duration {
-                tick = Instant::now();
+            // EK1100 is first SubDevice, EL2889 is second
+            let el2889 = slow_outputs
+                .subdevice(&maindevice_slow, 1)
+                .expect("EL2889 not present!");
 
-                let el2889 = slow_outputs
-                    .subdevice(&maindevice_slow, 1)
-                    .expect("EL2889 not present!");
+            // Set initial output state
+            el2889.outputs_raw_mut()[0] = 0x01;
+            el2889.outputs_raw_mut()[1] = 0x80;
 
-                let mut o = el2889.outputs_raw_mut();
+            loop {
+                let Ok(_) = slow_outputs.tx_rx(&maindevice_slow).await else {
+                    break;
+                };
 
-                // Make a nice pattern on EL2889 LEDs
-                o[0] = o[0].rotate_left(1);
-                o[1] = o[1].rotate_right(1);
-            }
+                // Increment every output byte for every SubDevice by one
+                if tick.elapsed() > slow_duration {
+                    tick = Instant::now();
 
-            slow_cycle_time.tick().await;
-        }
-    });
+                    let el2889 = slow_outputs
+                        .subdevice(&maindevice_slow, 1)
+                        .expect("EL2889 not present!");
 
-    let fast_task = tokio::spawn(async move {
-        let fast_outputs = fast_outputs
-            .into_op(&maindevice)
-            .await
-            .expect("PRE-OP -> OP");
+                    let mut o = el2889.outputs_raw_mut();
 
-        let mut fast_cycle_time = tokio::time::interval(Duration::from_millis(5));
-        fast_cycle_time.set_missed_tick_behavior(MissedTickBehavior::Skip);
-
-        loop {
-            let Ok(_) = fast_outputs.tx_rx(&maindevice).await else {
-                break;
-            };
-
-            // Increment every output byte for every SubDevice by one
-            for subdevice in fast_outputs.iter(&maindevice) {
-                let mut o = subdevice.outputs_raw_mut();
-
-                for byte in o.iter_mut() {
-                    *byte = byte.wrapping_add(1);
+                    // Make a nice pattern on EL2889 LEDs
+                    o[0] = o[0].rotate_left(1);
+                    o[1] = o[1].rotate_right(1);
                 }
+
+                slow_cycle_time.next().await;
             }
+        };
 
-            fast_cycle_time.tick().await;
-        }
-    });
+        let fast_task = async move {
+            let fast_outputs = fast_outputs
+                .into_op(&maindevice)
+                .await
+                .expect("PRE-OP -> OP");
 
-    let (slow, fast) = tokio::join!(slow_task, fast_task);
+            let mut fast_cycle_time = smol::Timer::interval(Duration::from_millis(5));
 
-    slow.expect("slow task failed");
-    fast.expect("fast task failed");
+            loop {
+                let Ok(_) = fast_outputs.tx_rx(&maindevice).await else {
+                    break;
+                };
 
-    Ok(())
+                // Increment every output byte for every SubDevice by one
+                for subdevice in fast_outputs.iter(&maindevice) {
+                    let mut o = subdevice.outputs_raw_mut();
+
+                    for byte in o.iter_mut() {
+                        *byte = byte.wrapping_add(1);
+                    }
+                }
+
+                fast_cycle_time.next().await;
+            }
+        };
+
+        let (_slow, _fast) = smol::future::zip(slow_task, fast_task).await;
+
+        Ok(())
+    })
 }
